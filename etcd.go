c135c1e299cb19d6e9a22f1bae36f503bbe58341
Xiang Li <xiang.li@coreos.com>
2013-07-11 14:56:16 -0700

    add comment for HTTPTIMEOUT constant

--- etcd.go
+++ etcd.go
@@ -91,6 +91,8 @@ const (
 	ELECTIONTIMTOUT  = 200 * time.Millisecond
 	HEARTBEATTIMEOUT = 50 * time.Millisecond
 	// Timeout for internal raft http connection
+	// The origin timeout for http is 45 seconds 
+	// which is too long for our usage.
  	HTTPTIMEOUT      = time.Second
 )
 
