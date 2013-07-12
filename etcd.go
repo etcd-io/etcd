00d2d6eb4eaec5ed56e8bda5d14dfc426a8d9e5b
Xiang Li <xiang.li@coreos.com>
2013-07-12 08:56:10 -0700

    gofmt

--- etcd.go
+++ etcd.go
@@ -90,9 +90,9 @@ const (
 const (
 	ELECTIONTIMTOUT  = 200 * time.Millisecond
 	HEARTBEATTIMEOUT = 50 * time.Millisecond
-	
+
 	// Timeout for internal raft http connection
-	// The original timeout for http is 45 seconds 
+	// The original timeout for http is 45 seconds
 	// which is too long for our usage.
 	HTTPTIMEOUT = time.Second
 )
