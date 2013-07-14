346da34a0002ecb0f0edbb52e6b354a5c355b456
Xiang Li <xiang.li@coreos.com>
2013-07-14 15:30:16 -0700

    fix a spelling error

--- error.go
+++ error.go
@@ -20,7 +20,7 @@ func init() {
 	errors[203] = "The given index in POST form is not a number"
 	// raft related errors
 	errors[300] = "Raft Internal Error"
-	errors[301] = "Durning Leader Election"
+	errors[301] = "During Leader Election"
 }
 
 type jsonError struct {
