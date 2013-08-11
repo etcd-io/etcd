8f3e6f340f5f607416aee51968f818cb410e6ba9
Xiang Li <xiang.li@coreos.com>
2013-08-11 11:42:38 -0700

    remove duplicate codes

--- machines.go
+++ machines.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"net/url"
-	"path"
-)
-
 // machineNum returns the number of machines in the cluster
 func machineNum() int {
 	response, _ := etcdStore.RawGet("_etcd/machines")
