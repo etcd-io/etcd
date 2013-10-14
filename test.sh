fb7a91739ad9d7764d4959ecd797293a810d1731
Ben Johnson <benbjohnson@yahoo.com>
2013-10-14 14:20:50 -0600

    Remove functional tests.

--- test.sh
+++ test.sh
@@ -6,3 +6,7 @@
 # Run the tests!
 go test -i
 go test -v
+
+# Run the functional tests!
+go test -i github.com/coreos/etcd-test-runner
+ETCD_BIN_PATH=$(pwd)/etcd go test -v github.com/coreos/etcd-test-runner
