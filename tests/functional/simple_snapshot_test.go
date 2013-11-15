4f45de6953d73b5ef63a9b32ff2cafd08246bba4
Xiang Li <xiang.li@coreos.com>
2013-11-14 20:22:25 -0800

    test fix wrong assumption should be previous index + 501 + 6

--- tests/functional/simple_snapshot_test.go
+++ tests/functional/simple_snapshot_test.go
@@ -55,7 +55,7 @@ func TestSimpleSnapshot(t *testing.T) {
 
 	index, _ := strconv.Atoi(snapshots[0].Name()[2:5])
 
-	if index < 507 || index > 510 {
+	if index <= 507 || index >= 510 {
 		t.Fatal("wrong name of snapshot :", snapshots[0].Name())
 	}
 
@@ -87,7 +87,7 @@ func TestSimpleSnapshot(t *testing.T) {
 
 	index, _ = strconv.Atoi(snapshots[0].Name()[2:6])
 
-	if index < 1015 || index > 1018 {
+	if index <= 1014 || index >= 1017 {
 		t.Fatal("wrong name of snapshot :", snapshots[0].Name())
 	}
 }
