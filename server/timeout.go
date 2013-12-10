cb9f677cf6b842d73fb7c613e49da760c6ae3ef1
Brandon Philips <brandon@ifup.co>
2013-12-10 11:13:37 -0800

    chore(server): cleanup some whitespace

--- server/timeout.go
+++ server/timeout.go
@@ -7,7 +7,7 @@ import (
 const (
 	// The amount of time to elapse without a heartbeat before becoming a candidate
 	defaultElectionTimeout = 200 * time.Millisecond
-	
+
 	// The frequency by which heartbeats are sent to followers.
 	defaultHeartbeatTimeout = 50 * time.Millisecond
 )
