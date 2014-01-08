8d25dac1ba4c9e7e9d950f073711f153a629ba31
Ben Johnson <benbjohnson@yahoo.com>
2014-01-08 16:21:22 -0700

    Revert test.sh changes.

--- test.sh
+++ test.sh
@@ -5,10 +5,6 @@ if [ -z "$PKG" ]; then
     PKG="./store ./server ./server/v2/tests ./mod/lock/v2/tests"
 fi
 
-if [ -z "$RUN" ]; then
-    RUN="."
-fi
-
 # Get GOPATH, etc from build
 . ./build
 
@@ -19,9 +15,9 @@ export GOPATH="${PWD}"
 for i in $PKG
 do
     go test -i $i
-    go test -v -test.run=$RUN $i
+    go test -v $i
 done
 
 # Functional tests
 go test -i ./tests/functional
-ETCD_BIN_PATH=$(pwd)/etcd go test -v  -test.run=$RUN ./tests/functional
+ETCD_BIN_PATH=$(pwd)/etcd go test -v ./tests/functional
