2d3196c449e6c578902de9f6d7375e8122a31ac0
rwindelz <rwindelz@gmail.com>
2014-01-19 13:55:39 -0800

    fix(build.ps1): windows build
    remove repo symlink and replace with directory junction

--- build.ps1
+++ build.ps1
@@ -7,7 +7,11 @@ $env:ETCD_TARGET=$pwd.Path
 $ETCD_BASE=(Split-Path $ETCD_DIR -Parent)
 #
 # for now, get rid of etcd symlink in the repo
-del $ETCD_DIR
+if(test-path $ETCD_DIR -pathType leaf){
+    del $ETCD_DIR
+}
+#
+# replace etcd symlink with a directory junction
 if(-not(test-path $ETCD_DIR)){
 	mkdir -force "$ETCD_BASE" > $null
 	cmd /c 'mklink /J "%ETCD_DIR%" %ETCD_TARGET%'
