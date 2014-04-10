842dd5cab015f20e4e583b1ef0e8e1dae52d6a5b
Ed Rooth <ed.rooth@coreos.com>
2014-04-10 10:47:19 -0700

    chore(dashboard): remove old dashboard

--- mod/dashboard/app/scripts/controllers/root.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('etcdControlPanel')
-.controller('RootCtrl', function($rootScope, prefixUrl) {
-
-  // Expose prefixUrl() function to all.
-  $rootScope.prefixUrl = prefixUrl;
-
-});
