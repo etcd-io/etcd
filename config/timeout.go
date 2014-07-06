02c854717bf45bb7bb93da0d9ffe5162d2ad430b
Xiang Li <xiang.li@coreos.com>
2014-07-06 10:46:07 -0700

    config: make config a self-contained pkg

--- config/timeout.go
+++
@@ -1,9 +0,0 @@
-package config
-
-const (
-	// The amount of time (in ms) to elapse without a heartbeat before becoming a candidate
-	defaultElectionTimeout = 200
-
-	// The frequency (in ms) by which heartbeats are sent to followers.
-	defaultHeartbeatInterval = 50
-)
