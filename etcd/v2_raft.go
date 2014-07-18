b4cf146a528f401e7dec46d625a3ea2bf226cbe9
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2014-07-18 15:59:39 -0700

    server: notice 2013 -> 2014

--- etcd/v2_raft.go
+++ etcd/v2_raft.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2013 CoreOS Inc.
+Copyright 2014 CoreOS Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
