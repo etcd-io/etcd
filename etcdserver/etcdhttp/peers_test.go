908d326e229e213b9fd9aedd2a025022647c09d4
Xiang Li <xiang.li@coreos.com>
2014-09-08 20:50:35 -0700

    etcdhttp: use peer.String() in test

--- etcdserver/etcdhttp/peers_test.go
+++ etcdserver/etcdhttp/peers_test.go
@@ -5,17 +5,15 @@ import "testing"
 //TODO: full testing for peer set
 func TestPeerSet(t *testing.T) {
 	p := &Peers{}
-
-	p.Set("0x1=1.1.1.1")
-	if p.Pick(0x1) != "http://1.1.1.1" {
-		t.Errorf("pick = %s, want http://1.1.1.1", p.Pick(0x1))
+	tests := []string{
+		"1=1.1.1.1",
+		"2=2.2.2.2",
+		"1=1.1.1.1&1=1.1.1.2&2=2.2.2.2",
 	}
-
-	p.Set("0x2=2.2.2.2")
-	if p.Pick(0x1) != "" {
-		t.Errorf("pick = %s, want empty string", p.Pick(0x1))
-	}
-	if p.Pick(0x2) != "http://2.2.2.2" {
-		t.Errorf("pick = %s, want http://2.2.2.2", p.Pick(0x2))
+	for i, tt := range tests {
+		p.Set(tt)
+		if p.String() != tt {
+			t.Errorf("#%d: string = %s, want %s", i, p.String(), tt)
+		}
 	}
 }
