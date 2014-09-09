7cc6305436fada76eb7e385e3154a8e2ad2dda75
Xiang Li <xiang.li@coreos.com>
2014-09-08 18:47:40 -0700

    main: use etcd as prefix in log for main.go

--- main.go
+++ main.go
@@ -49,10 +49,10 @@ func main() {
 
 	if *dir == "" {
 		*dir = fmt.Sprintf("%v_etcd_data", *fid)
-		log.Printf("main: no data-dir is given, using default data-dir ./%s", *dir)
+		log.Printf("etcd: no data-dir is given, using default data-dir ./%s", *dir)
 	}
 	if err := os.MkdirAll(*dir, privateDirMode); err != nil {
-		log.Fatalf("main: cannot create data directory: %v", err)
+		log.Fatalf("etcd: cannot create data directory: %v", err)
 	}
 
 	n, w := startRaft(id, peers.Ids(), path.Join(*dir, "wal"))
