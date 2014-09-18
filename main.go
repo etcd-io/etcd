f0789e734928126716ef2c74e91b8bd24f77a62f
Jonathan Boulle <jonathanboulle@gmail.com>
2014-09-18 15:27:41 -0700

    main: add address validation for bind-addr flag

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"path"
@@ -200,12 +201,15 @@ func startProxy() {
 type Addrs []string
 
 // Set parses a command line set of listen addresses, formatted like:
-// 127.0.0.1:7001,unix:///var/run/etcd.sock,10.1.1.1:8080
+// 127.0.0.1:7001,10.1.1.2:80
 func (as *Addrs) Set(s string) error {
-	// TODO(jonboulle): validate things.
 	parsed := make([]string, 0)
-	for _, a := range strings.Split(s, ",") {
-		parsed = append(parsed, strings.TrimSpace(a))
+	for _, in := range strings.Split(s, ",") {
+		a := strings.TrimSpace(in)
+		if err := validateAddr(a); err != nil {
+			return err
+		}
+		parsed = append(parsed, a)
 	}
 	if len(parsed) == 0 {
 		return errors.New("no valid addresses given!")
@@ -216,6 +220,23 @@ func (as *Addrs) Set(s string) error {
 
 func (as *Addrs) String() string {
 	return strings.Join(*as, ",")
+}
+
+// validateAddr ensures that the provided string is a valid address. Valid
+// addresses are of the form IP:port.
+// Returns an error if the address is invalid, else nil.
+func validateAddr(s string) error {
+	parts := strings.SplitN(s, ":", 2)
+	if len(parts) != 2 {
+		return errors.New("bad format in address specification")
+	}
+	if net.ParseIP(parts[0]) == nil {
+		return errors.New("bad IP in address specification")
+	}
+	if _, err := strconv.Atoi(parts[1]); err != nil {
+		return errors.New("bad port in address specification")
+	}
+	return nil
 }
 
 // ProxyFlag implements the flag.Value interface.
