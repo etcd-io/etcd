34b2fecd28a90e84209df9a72e989cbe147bd25d
Jonathan Boulle <jonathanboulle@gmail.com>
2014-10-31 14:25:26 -0700

    integration: clean up licenses and docs

--- integration/doc.go
+++ integration/doc.go
@@ -1,25 +1,27 @@
-// Copyright 2014 CoreOS Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+   Copyright 2014 CoreOS, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 /*
-functional tests are built upon embeded etcd, and focus on etcd functional
-correctness.
+Package integration implements tests built upon embedded etcd, and focus on
+etcd correctness.
 
-Its goal:
-1. it tests the whole code base except the command line parse.
-2. it is able to check internal data, including raft, store and etc.
-3. it is based on goroutine, which is faster than process.
-4. it mainly tests user behavior and user-facing API.
+Features/goals of the integration tests:
+1. test the whole code base except command-line parsing.
+2. check internal data, including raft, store and etc.
+3. based on goroutines, which is faster than process.
+4. mainly tests user behavior and user-facing API.
 */
 package integration
