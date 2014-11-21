03c8881e35ed649f4bdd8197829edd562270568c
Ben Darnell <bdarnell@squareup.com>
2014-11-21 16:37:55 -0500

    Fix TestSlowNodeRestore

--- raft/raft_test.go
+++ raft/raft_test.go
@@ -1179,7 +1179,7 @@ func TestSlowNodeRestore(t *testing.T) {
 	}
 	lead := nt.peers[1].(*raft)
 	nextEnts(lead, nt.storage[1])
-	//lead.compact(lead.raftLog.applied, lead.nodes(), nil)
+	nt.storage[1].Compact(lead.raftLog.applied, &pb.ConfState{Nodes: lead.nodes()}, nil)
 
 	nt.recover()
 	// trigger a snapshot
