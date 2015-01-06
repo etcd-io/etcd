0afbca40905478f4eadceb0a9576ce0e1ed33876
Xiang Li <xiang.li@coreos.com>
2015-01-06 11:01:11 -0800

    etcdmain: add readtimeout for http server

--- etcdmain/http.go
+++ etcdmain/http.go
@@ -5,17 +5,19 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
 // serveHTTP accepts incoming HTTP connections on the listener l,
 // creating a new service goroutine for each. The service goroutines
 // read requests and then call handler to reply to them.
-func serveHTTP(l net.Listener, handler http.Handler) error {
+func serveHTTP(l net.Listener, handler http.Handler, readTimeout time.Duration) error {
 	logger := log.New(ioutil.Discard, "etcdhttp", 0)
 	// TODO: add debug flag; enable logging when debug flag is set
 	srv := &http.Server{
-		Handler:  handler,
-		ErrorLog: logger, // do not log user error
+		Handler:     handler,
+		ReadTimeout: readTimeout,
+		ErrorLog:    logger, // do not log user error
 	}
 	return srv.Serve(l)
 }
