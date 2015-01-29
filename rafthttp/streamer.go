99840c96974fac56940efe32b4623337f20c8b70
Xiang Li <xiang.li@coreos.com>
2015-01-29 15:59:58 -0800

    *: cleanup import

--- rafthttp/streamer.go
+++ rafthttp/streamer.go
@@ -27,11 +27,10 @@ import (
 	"sync"
 	"time"
 
+	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
 	"github.com/coreos/etcd/etcdserver/stats"
 	"github.com/coreos/etcd/pkg/types"
 	"github.com/coreos/etcd/raft/raftpb"
-
-	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
 )
 
 const (
