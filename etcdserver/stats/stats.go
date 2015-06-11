f013a627a4177f4dfc134a1914696f1cfc31429d
Xiang Li <xiangli.cs@gmail.com>
2015-06-11 09:40:44 -0700

    etcdserver/stats: use leveled log

--- etcdserver/stats/stats.go
+++ etcdserver/stats/stats.go
@@ -14,6 +14,12 @@
 
 package stats
 
+import "github.com/coreos/pkg/capnslog"
+
+var (
+	plog = capnslog.NewPackageLogger("github.com/coreos/etcd/etcdserver", "stats")
+)
+
 type Stats interface {
 	// SelfStats returns the struct representing statistics of this server
 	SelfStats() []byte
