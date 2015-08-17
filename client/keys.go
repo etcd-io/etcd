a364af72afe24c4237c852700f35f5dfb64a90ee
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2015-08-16 17:41:14 -0700

    client: use ugorij/go/codec to unmarshal key response
    This change speeds up response unmarshal ~2x:
    
    ```
    BenchmarkSmallResponseUnmarshal	   20000	     75243 ns/op
    BenchmarkManySmallResponseUnmarshal	     200	   6629661 ns/op
    BenchmarkMediumResponseUnmarshal	    1000	   1359041 ns/op
    BenchmarkLargeResponseUnmarshal	      20	  61600978 ns/op
    ```

--- client/keys.go
+++ client/keys.go
@@ -14,6 +14,8 @@
 
 package client
 
+//go:generate codecgen -r "Node|Response" -o keys.generated.go keys.go
+
 import (
 	"encoding/json"
 	"errors"
@@ -26,6 +28,7 @@ import (
 	"time"
 
 	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
+	"github.com/ugorji/go/codec"
 )
 
 const (
@@ -609,7 +612,7 @@ func unmarshalHTTPResponse(code int, header http.Header, body []byte) (res *Resp
 
 func unmarshalSuccessfulKeysResponse(header http.Header, body []byte) (*Response, error) {
 	var res Response
-	err := json.Unmarshal(body, &res)
+	err := codec.NewDecoderBytes(body, new(codec.JsonHandle)).Decode(&res)
 	if err != nil {
 		return nil, ErrInvalidJSON
 	}
