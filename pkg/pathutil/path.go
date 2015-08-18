4778d780a8e00cd6f6fc2c92545d4a9d6254f012
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2015-08-18 11:48:22 -0700

    pkg/pathutil: change copyright for path.go
    The file only contains the function that is borrowed from std http lib,
    so we use their copyright.

--- pkg/pathutil/path.go
+++ pkg/pathutil/path.go
@@ -1,16 +1,6 @@
-// Copyright 2015 CoreOS, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package pathutil
 
@@ -21,6 +11,7 @@ import "path"
 // 2. replace multiple slashes with a single slash
 // 3. replace each '.' '..' path name element with equivalent one
 // 4. keep the trailing slash
+// The function is borrowed from stdlib http.cleanPath in server.go.
 func CanonicalURLPath(p string) string {
 	if p == "" {
 		return "/"
