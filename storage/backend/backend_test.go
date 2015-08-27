19a28c8efdc40fb4d173822915401f2e783b2345
Matt Keller <m@cognusion.com>
2015-08-27 15:20:29 -0400

    storage: Fixed backend test
    ./backend_test.go:23: multiple-value batchTx.UnsafeRange() in single-value context

--- storage/backend/backend_test.go
+++ storage/backend/backend_test.go
@@ -20,7 +20,7 @@ func TestBackendPut(t *testing.T) {
 	batchTx.UnsafeCreateBucket([]byte("test"))
 
 	batchTx.UnsafePut([]byte("test"), []byte("foo"), v)
-	gv := batchTx.UnsafeRange([]byte("test"), v, nil, -1)
+	_, gv := batchTx.UnsafeRange([]byte("test"), v, nil, -1)
 	if !reflect.DeepEqual(gv[0], v) {
 		t.Errorf("v = %s, want %s", string(gv[0]), string(v))
 	}
