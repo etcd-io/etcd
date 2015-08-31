4b295ffa2841fc75ab51943d58ccb1fb3d837359
chai2010 <chaishushan@gmail.com>
2015-08-31 18:01:51 +0800

    sync copyright

--- main.go
+++ main.go
@@ -1,20 +1,16 @@
-// +build !windows
-
-/*
-   Copyright 2014 CoreOS, Inc.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+// Copyright 2015 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // Package main is a simple wrapper of the real etcd entrypoint package
 // (located at github.com/coreos/etcd/etcdmain) to ensure that etcd is still
@@ -25,11 +21,11 @@
 // etcd binary, work in the `github.com/coreos/etcd/etcdmain` package.
 //
 
+// +build !windows
+
 package main
 
-import (
-	"github.com/coreos/etcd/etcdmain"
-)
+import "github.com/coreos/etcd/etcdmain"
 
 func main() {
 	etcdmain.Main()
