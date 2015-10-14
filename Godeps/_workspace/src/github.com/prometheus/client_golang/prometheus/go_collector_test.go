0dd0968b5ed40ff963ce3de2188aac4b31551b4b
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2015-10-14 10:35:41 -0700

    Godeps: update prometheus dependency
    prometheus updates its directory layout
    (https://github.com/prometheus/client_golang#where-is-model-extraction-and-text)
    and makes Godeps restore/save unable to work.
    
    Remove all prometheus dependency manually and godep save again to fix
    this problem.

--- Godeps/_workspace/src/github.com/prometheus/client_golang/prometheus/go_collector_test.go
+++ Godeps/_workspace/src/github.com/prometheus/client_golang/prometheus/go_collector_test.go
@@ -50,6 +50,10 @@ func TestGoCollector(t *testing.T) {
 					t.Errorf("want 1 new goroutine, got %d", diff)
 				}
 
+				// GoCollector performs two sends per call.
+				// On line 27 we need to receive the second send
+				// to shut down cleanly.
+				<-ch
 				return
 			}
 		case <-time.After(1 * time.Second):
