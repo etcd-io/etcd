627cbb0a9b23108e0e764e156eb170abe642f79d
sayden <mariocaster@gmail.com>
2015-12-29 20:58:02 +0100

    Redundant comments removed from setGetKey example

--- client/examples/setGetKey/setGetKey.go
+++ client/examples/setGetKey/setGetKey.go
@@ -26,7 +26,8 @@ func main() {
 	cfg := client.Config{
 		Endpoints: []string{"http://127.0.0.1:2379"},
 		Transport: client.DefaultTransport,
-		// set timeout per request to fail fast when the target endpoint is unavailable
+		// set timeout per request to fail fast when the target endpoint
+		// is unavailable
 		HeaderTimeoutPerRequest: time.Second,
 	}
 	c, err := client.New(cfg)
@@ -34,24 +35,20 @@ func main() {
 		log.Fatal(err)
 	}
 	kapi := client.NewKeysAPI(c)
-	// set "/foo" key with "bar" value
+
 	log.Print("Setting '/foo' key with 'bar' value")
 	resp, err := kapi.Set(context.Background(), "/foo", "bar", nil)
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		// print common key info
-		log.Printf("Set is done. Metadata is %q\n", resp)
 	}
-	// get "/foo" key's value
+	log.Printf("Set is done. Metadata is %q\n", resp)
+
 	log.Print("Getting '/foo' key value")
 	resp, err = kapi.Get(context.Background(), "/foo", nil)
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		// print common key info
-		log.Printf("Get is done. Metadata is %q\n", resp)
-		// print value
-		log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
 	}
+
+	log.Printf("Get is done. Metadata is %q\n", resp)
+	log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
 }
