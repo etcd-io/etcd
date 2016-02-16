0cf9cde1b437ebe175dffeff5c1a28d29a34c80e
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2016-02-16 11:42:29 +0900

    etcdserver, auth: remove obsolete mutex
    The removed mutex is obsolete because the enabled field is removed
    in the commit b2d2c79a2fcb20f6c64eeb95d1e7d1ddbcd453b1.

--- etcdserver/auth/auth.go
+++ etcdserver/auth/auth.go
@@ -23,7 +23,6 @@ import (
 	"reflect"
 	"sort"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/coreos/etcd/Godeps/_workspace/src/github.com/coreos/pkg/capnslog"
@@ -101,8 +100,6 @@ type store struct {
 	server      doer
 	timeout     time.Duration
 	ensuredOnce bool
-
-	mu sync.Mutex // protect enabled
 
 	PasswordStore
 }
@@ -409,9 +406,6 @@ func (s *store) UpdateRole(role Role) (Role, error) {
 }
 
 func (s *store) AuthEnabled() bool {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
 	return s.detectAuth()
 }
 
@@ -419,9 +413,6 @@ func (s *store) EnableAuth() error {
 	if s.AuthEnabled() {
 		return authErr(http.StatusConflict, "already enabled")
 	}
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
 
 	if _, err := s.GetUser("root"); err != nil {
 		return authErr(http.StatusConflict, "No root user available, please create one")
@@ -447,9 +438,6 @@ func (s *store) DisableAuth() error {
 	if !s.AuthEnabled() {
 		return authErr(http.StatusConflict, "already disabled")
 	}
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
 
 	err := s.disableAuth()
 	if err == nil {
