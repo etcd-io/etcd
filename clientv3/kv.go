2c83362e63e6b796ea988442483673056c567455
Anthony Romano <anthony.romano@coreos.com>
2016-03-27 02:30:39 -0700

    clientv3: fix race in KV reconnection logic

--- clientv3/kv.go
+++ clientv3/kv.go
@@ -183,13 +183,17 @@ func (kv *kv) Do(ctx context.Context, op Op) (OpResponse, error) {
 }
 
 func (kv *kv) switchRemote(prevErr error) error {
+	// Usually it's a bad idea to lock on network i/o but here it's OK
+	// since the link is down and new requests can't be processed anyway.
+	// Likewise, if connecting stalls, closing the Client can break the
+	// lock via context cancelation.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
 	newConn, err := kv.c.retryConnection(kv.conn, prevErr)
 	if err != nil {
 		return err
 	}
-
-	kv.mu.Lock()
-	defer kv.mu.Unlock()
 
 	kv.conn = newConn
 	kv.remote = pb.NewKVClient(kv.conn)
