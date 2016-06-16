d2912e3068afdbfa8f364ec5b80054c3d5887f8c
Hitoshi Mitake <mitake.hitoshi@gmail.com>
2016-06-15 21:31:07 -0700

    etcdserver, pkg: skip needless log entry applying
    This commit lets etcdserver skip needless log entry applying. If the
    result of log applying isn't required by the node (client that issued
    the request isn't talking with the node) and the operation has no side
    effects, applying can be skipped.
    
    It would contribute to reduce disk I/O on followers and be useful for
    a cluster that processes much serializable get.

--- pkg/wait/wait_test.go
+++ pkg/wait/wait_test.go
@@ -77,3 +77,26 @@ func TestTriggerDupSuppression(t *testing.T) {
 		t.Errorf("unexpected non-nil value: %v (%T)", g, g)
 	}
 }
+
+func TestIsRegistered(t *testing.T) {
+	wt := New()
+
+	wt.Register(0)
+	wt.Register(1)
+	wt.Register(2)
+
+	for i := uint64(0); i < 3; i++ {
+		if !wt.IsRegistered(i) {
+			t.Errorf("event ID %d isn't registered", i)
+		}
+	}
+
+	if wt.IsRegistered(4) {
+		t.Errorf("event ID 4 shouldn't be registered")
+	}
+
+	wt.Trigger(0, "foo")
+	if wt.IsRegistered(0) {
+		t.Errorf("event ID 0 is already triggered, shouldn't be registered")
+	}
+}
