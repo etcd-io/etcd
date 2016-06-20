6fe4d9d30a1c130e457a7bfdf033f5b37f7a5797
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2016-06-20 16:31:22 +0900

    e2e: check runtime permission changing
    This commit adds extends the test for checking runtime permission
    grant/revoke.

--- e2e/ctl_v3_role_test.go
+++ e2e/ctl_v3_role_test.go
@@ -14,7 +14,10 @@
 
 package e2e
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCtlV3RoleAdd(t *testing.T)          { testCtl(t, roleAddTest) }
 func TestCtlV3RoleAddNoTLS(t *testing.T)     { testCtl(t, roleAddTest, withCfg(configNoTLS)) }
@@ -94,4 +97,65 @@ func ctlV3Role(cx ctlCtx, args []string, expStr string) error {
 	cmdArgs = append(cmdArgs, args...)
 
 	return spawnWithExpect(cmdArgs, expStr)
+}
+
+func ctlV3RoleGrantPermission(cx ctlCtx, rolename string, perm grantingPerm) error {
+	cmdArgs := append(cx.PrefixArgs(), "role", "grant-permission")
+	cmdArgs = append(cmdArgs, rolename)
+	cmdArgs = append(cmdArgs, grantingPermToArgs(perm)...)
+
+	proc, err := spawnCmd(cmdArgs)
+	if err != nil {
+		return err
+	}
+
+	expStr := fmt.Sprintf("Role %s updated", rolename)
+	_, err = proc.Expect(expStr)
+	return err
+}
+
+func ctlV3RoleRevokePermission(cx ctlCtx, rolename string, key, rangeEnd string) error {
+	cmdArgs := append(cx.PrefixArgs(), "role", "revoke-permission")
+	cmdArgs = append(cmdArgs, rolename)
+	cmdArgs = append(cmdArgs, key)
+	if len(rangeEnd) != 0 {
+		cmdArgs = append(cmdArgs, rangeEnd)
+	}
+
+	proc, err := spawnCmd(cmdArgs)
+	if err != nil {
+		return err
+	}
+
+	expStr := fmt.Sprintf("Permission of key %s is revoked from role %s", key, rolename)
+	_, err = proc.Expect(expStr)
+	return err
+}
+
+type grantingPerm struct {
+	read     bool
+	write    bool
+	key      string
+	rangeEnd string
+}
+
+func grantingPermToArgs(perm grantingPerm) []string {
+	permstr := ""
+
+	if perm.read {
+		permstr += "read"
+	}
+
+	if perm.write {
+		permstr += "write"
+	}
+
+	if len(permstr) == 0 {
+		panic("invalid granting permission")
+	}
+
+	if len(perm.rangeEnd) == 0 {
+		return []string{permstr, perm.key}
+	}
+	return []string{permstr, perm.key, perm.rangeEnd}
 }
