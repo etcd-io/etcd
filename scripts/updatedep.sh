0fb2ce3ca3d0a456b97e17f53866c640cbf24c6f
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-20 10:33:58 -0700

    scripts/updatedep: manual replace golang/protobuf

--- scripts/updatedep.sh
+++ scripts/updatedep.sh
@@ -10,3 +10,7 @@ rm -rf cmd/Godeps
 rm vendor
 mv Godeps cmd/
 
+# to use 'gogo/protobuf/jsonpb'
+# cmd/vendor/github.com/grpc-ecosystem/grpc-gateway/runtime/marshal_jsonpb.go
+find cmd/vendor/github.com/grpc-ecosystem/grpc-gateway/ -type f -exec sed -i 's/github.com\/golang\/protobuf\/jsonpb/github.com\/gogo\/protobuf\/jsonpb/g' {} \;
+
