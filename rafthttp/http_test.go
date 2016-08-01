911dcc9386291bdc7ba169531f976296f73b182c
Anthony Romano <anthony.romano@coreos.com>
2016-07-31 20:08:27 -0700

    rafthttp: close http socket when pipeline handler gets a raft error
    Otherwise the http stream remains open and keeps receiving raft messages.
    This can lead to "raft: stopped" log spam on closing an embedded server.
    
    Fixes #5981

--- rafthttp/http_test.go
+++ rafthttp/http_test.go
@@ -152,7 +152,18 @@ func TestServeRaftPrefix(t *testing.T) {
 		req.Header.Set("X-Server-Version", version.Version)
 		rw := httptest.NewRecorder()
 		h := newPipelineHandler(NewNopTransporter(), tt.p, types.ID(0))
-		h.ServeHTTP(rw, req)
+
+		// goroutine because the handler panics to disconnect on raft error
+		donec := make(chan struct{})
+		go func() {
+			defer func() {
+				recover()
+				close(donec)
+			}()
+			h.ServeHTTP(rw, req)
+		}()
+		<-donec
+
 		if rw.Code != tt.wcode {
 			t.Errorf("#%d: got code=%d, want %d", i, rw.Code, tt.wcode)
 		}
