f2b953d4f7c967a8439626a100d769273671e705
Gyu-Ho Lee <gyuhox@gmail.com>
2016-10-14 14:53:13 -0700

    version: bump up to v3.1.0-rc.0+git

--- version/version.go
+++ version/version.go
@@ -24,7 +24,7 @@ import (
 var (
 	// MinClusterVersion is the min cluster version this etcd binary is compatible with.
 	MinClusterVersion = "3.0.0"
-	Version           = "3.1.0-rc.0"
+	Version           = "3.1.0-rc.0+git"
 
 	// Git SHA Value will be set during build
 	GitSHA = "Not provided (use ./build instead of go build)"
