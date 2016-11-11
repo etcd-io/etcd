1ef243e436e946c193833cc86d53f2ac098a9a0d
Xiang Li <xiangli.cs@gmail.com>
2016-11-11 09:26:02 -0800

    etcdctl: etcdctl v3 should print out its API version

--- version/version.go
+++ version/version.go
@@ -19,16 +19,26 @@ package version
 import (
 	"fmt"
 	"strings"
+
+	"github.com/coreos/go-semver/semver"
 )
 
 var (
 	// MinClusterVersion is the min cluster version this etcd binary is compatible with.
 	MinClusterVersion = "3.0.0"
 	Version           = "3.1.0-rc.0+git"
+	APIVersion        = "unknown"
 
 	// Git SHA Value will be set during build
 	GitSHA = "Not provided (use ./build instead of go build)"
 )
+
+func init() {
+	ver, err := semver.NewVersion(Version)
+	if err == nil {
+		APIVersion = fmt.Sprintf("%d.%d", ver.Major, ver.Minor)
+	}
+}
 
 type Versions struct {
 	Server  string `json:"etcdserver"`
