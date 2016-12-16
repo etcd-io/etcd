219a4e9ad5d1b5d930f20d4cad5292876e94e422
Anthony Romano <anthony.romano@coreos.com>
2016-12-16 00:28:17 -0800

    clientv3: don't reset keepalive stream on grant failure
    Was triggering cancelation errors on outstanding KeepAlives if Grant
    had to retry.

--- clientv3/lease.go
+++ clientv3/lease.go
@@ -143,9 +143,6 @@ func (l *lessor) Grant(ctx context.Context, ttl int64) (*LeaseGrantResponse, err
 		if isHaltErr(cctx, err) {
 			return nil, toErr(ctx, err)
 		}
-		if nerr := l.newStream(); nerr != nil {
-			return nil, nerr
-		}
 	}
 }
 
