0f53ad0b84ad535eef02be2931bd2012687dcb45
felixoid <mr.felixoid@gmail.com>
2017-01-31 21:48:36 +0300

    netutil: add dualstack to linux_route
    in v3.1.0 netutil couldn't get default interface for ipv6only hosts
    
    Fixes #7219

--- pkg/netutil/routes_linux_test.go
+++ pkg/netutil/routes_linux_test.go
@@ -19,9 +19,17 @@ package netutil
 import "testing"
 
 func TestGetDefaultInterface(t *testing.T) {
-	ifc, err := GetDefaultInterface()
+	ifc, err := GetDefaultInterfaces()
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("default network interface: %q\n", ifc)
+	t.Logf("default network interfaces: %+v\n", ifc)
+}
+
+func TestGetDefaultHost(t *testing.T) {
+	ip, err := GetDefaultHost()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("default ip: %v", ip)
 }
