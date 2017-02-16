4fb8d30f0a19f4ca6c1c4112e941ef80e0032b7f
Xiang <xiangli.cs@gmail.com>
2017-02-16 11:13:40 -0800

    clientv3: fix lease keepalive duration

--- clientv3/lease.go
+++ clientv3/lease.go
@@ -407,7 +407,7 @@ func (l *lessor) recvKeepAlive(resp *pb.LeaseKeepAliveResponse) {
 	}
 
 	// send update to all channels
-	nextKeepAlive := time.Now().Add(time.Duration(karesp.TTL/3+1) * time.Second)
+	nextKeepAlive := time.Now().Add(time.Duration(karesp.TTL+2) / 3 * time.Second)
 	ka.deadline = time.Now().Add(time.Duration(karesp.TTL) * time.Second)
 	for _, ch := range ka.chs {
 		select {
