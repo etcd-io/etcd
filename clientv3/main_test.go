593489d45426615cd784761895f6c14ae6e8cdff
Anthony Romano <anthony.romano@coreos.com>
2017-03-10 12:21:44 -0800

    clientv3: use CheckAfterTest after terminating cluster
    AfterTest() has a delay that waits for runtime goroutines to exit;
    CheckLeakedGoroutine does not. Since the test runner manages the
    test cluster for examples, there is no delay between terminating
    the cluster and checking for leaked goroutines. Instead, apply
    Aftertest checking before running CheckLeakedGoroutine to let runtime
    http goroutines finish.

--- clientv3/main_test.go
+++ clientv3/main_test.go
@@ -15,10 +15,12 @@
 package clientv3_test
 
 import (
+	"fmt"
 	"os"
 	"regexp"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/coreos/etcd/auth"
 	"github.com/coreos/etcd/integration"
@@ -50,6 +52,10 @@ func TestMain(m *testing.M) {
 		}
 		v = m.Run()
 		clus.Terminate(nil)
+		if err := testutil.CheckAfterTest(time.Second); err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(1)
+		}
 	} else {
 		v = m.Run()
 	}
