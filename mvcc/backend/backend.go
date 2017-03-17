8a3fee15a356a2bd7db40a909abe6e2b1d7ffb45
Anthony Romano <anthony.romano@coreos.com>
2017-03-16 20:20:49 -0700

    etcdserver, backend: only warn if exceeding max quota

--- mvcc/backend/backend.go
+++ mvcc/backend/backend.go
@@ -43,15 +43,6 @@ var (
 	plog = capnslog.NewPackageLogger("github.com/coreos/etcd", "mvcc/backend")
 )
 
-const (
-	// DefaultQuotaBytes is the number of bytes the backend Size may
-	// consume before exceeding the space quota.
-	DefaultQuotaBytes = int64(2 * 1024 * 1024 * 1024) // 2GB
-	// MaxQuotaBytes is the maximum number of bytes suggested for a backend
-	// quota. A larger quota may lead to degraded performance.
-	MaxQuotaBytes = int64(8 * 1024 * 1024 * 1024) // 8GB
-)
-
 type Backend interface {
 	ReadTx() ReadTx
 	BatchTx() BatchTx
