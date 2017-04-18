5161b747991da9137ac33868a1a68478b9149d75
Gyu-Ho Lee <gyuhox@gmail.com>
2017-04-18 10:00:09 -0700

    pkg/debugutil: add 'mutex' profiler (Go 1.8+)
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- pkg/debugutil/pprof.go
+++ pkg/debugutil/pprof.go
@@ -17,12 +17,19 @@ package debugutil
 import (
 	"net/http"
 	"net/http/pprof"
+	"runtime"
 )
 
 const HTTPPrefixPProf = "/debug/pprof"
 
 // PProfHandlers returns a map of pprof handlers keyed by the HTTP path.
 func PProfHandlers() map[string]http.Handler {
+	// set only when there's no existing setting
+	if runtime.SetMutexProfileFraction(-1) == 0 {
+		// 1 out of 5 mutex events are reported, on average
+		runtime.SetMutexProfileFraction(5)
+	}
+
 	m := make(map[string]http.Handler)
 
 	m[HTTPPrefixPProf+"/"] = http.HandlerFunc(pprof.Index)
@@ -34,6 +41,7 @@ func PProfHandlers() map[string]http.Handler {
 	m[HTTPPrefixPProf+"/goroutine"] = pprof.Handler("goroutine")
 	m[HTTPPrefixPProf+"/threadcreate"] = pprof.Handler("threadcreate")
 	m[HTTPPrefixPProf+"/block"] = pprof.Handler("block")
+	m[HTTPPrefixPProf+"/mutex"] = pprof.Handler("mutex")
 
 	return m
 }
