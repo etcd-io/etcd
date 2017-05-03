f6d0dda1871e0e5fd075eb6c2db0e9d9f5f75478
Anthony Romano <anthony.romano@coreos.com>
2017-05-03 13:22:45 -0700

    clientv3/integration: drain keepalives before waiting for leader loss
    500ms keepalive delay on proxy side causes client to sometimes send
    a second keepalive since it waits more than 500ms for the first response.
    
    Fixes #7658

--- clientv3/integration/lease_test.go
+++ clientv3/integration/lease_test.go
@@ -729,6 +729,12 @@ func TestLeaseWithRequireLeader(t *testing.T) {
 	}
 
 	clus.Members[1].Stop(t)
+	// kaReqLeader may issue multiple requests while waiting for the first
+	// response from proxy server; drain any stray keepalive responses
+	time.Sleep(100 * time.Millisecond)
+	for len(kaReqLeader) > 0 {
+		<-kaReqLeader
+	}
 
 	select {
 	case resp, ok := <-kaReqLeader:
