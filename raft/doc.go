b4267f42d28c05f858547f21b351e6bd51a4e22a
irfan sharif <irfanmahmoudsharif@gmail.com>
2017-07-16 14:05:57 -0400

    raft: wrap raft/{README,design}.md to 80 characters
    + Fix typos/comments.

--- raft/doc.go
+++ raft/doc.go
@@ -260,7 +260,7 @@ stale log entries:
 	'MsgPreVote' and 'MsgPreVoteResp' are used in an optional two-phase election
 	protocol. When Config.PreVote is true, a pre-election is carried out first
 	(using the same rules as a regular election), and no node increases its term
-	number unless the pre-election indicates that the campaigining node would win.
+	number unless the pre-election indicates that the campaigning node would win.
 	This minimizes disruption when a partitioned node rejoins the cluster.
 
 	'MsgSnap' requests to install a snapshot message. When a node has just
