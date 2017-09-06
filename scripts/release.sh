9abe9da9db5bd20371e481ac2ad3538be8d952ff
Anthony Romano <anthony.romano@coreos.com>
2017-09-06 10:38:08 -0700

    *: fix shellcheck warnings
    Fixes scripts and removes shellcheck warning suppressions.
    
    * regexp warnings
    * use ./*glob* so names don't become options
    * use $(..) instead of legacy `..`
    * read with -r to avoid mangling backslashes
    * double quote to prevent globbing and word splitting

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,18 +23,18 @@ fi
 
 ETCD_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
-pushd ${ETCD_ROOT} >/dev/null
+pushd "${ETCD_ROOT}" >/dev/null
 	echo Building etcd binary...
-	./scripts/build-binary ${VERSION}
+	./scripts/build-binary "${VERSION}"
 
 	# ppc64le not yet supported by acbuild.
 	for TARGET_ARCH in "amd64" "arm64"; do
 		echo Building ${TARGET_ARCH} aci image...
-		GOARCH=${TARGET_ARCH} BINARYDIR=release/etcd-${VERSION}-linux-${TARGET_ARCH} BUILDDIR=release ./scripts/build-aci ${VERSION}
+		GOARCH=${TARGET_ARCH} BINARYDIR=release/etcd-${VERSION}-linux-${TARGET_ARCH} BUILDDIR=release ./scripts/build-aci "${VERSION}"
 	done
 
 	for TARGET_ARCH in "amd64" "arm64" "ppc64le"; do
 		echo Building ${TARGET_ARCH} docker image...
-		GOARCH=${TARGET_ARCH} BINARYDIR=release/etcd-${VERSION}-linux-${TARGET_ARCH} BUILDDIR=release ./scripts/build-docker ${VERSION}
+		GOARCH=${TARGET_ARCH} BINARYDIR=release/etcd-${VERSION}-linux-${TARGET_ARCH} BUILDDIR=release ./scripts/build-docker "${VERSION}"
 	done
 popd >/dev/null
