18ba4d60ec5bd74f1e71b4d1436739dce60280a7
Gyu-Ho Lee <gyuhox@gmail.com>
2017-09-08 13:29:58 -0700

    v3rpc/rpctypes: use grpc.status for errors
    grpc.Code, grpc.ErrorDesc, grpc.Errorf have been deprecated.
    
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- etcdserver/api/v3rpc/rpctypes/error_test.go
+++ etcdserver/api/v3rpc/rpctypes/error_test.go
@@ -17,26 +17,28 @@ package rpctypes
 import (
 	"testing"
 
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestConvert(t *testing.T) {
-	e1 := grpc.Errorf(codes.InvalidArgument, "etcdserver: key is not provided")
+	e1 := status.New(codes.InvalidArgument, "etcdserver: key is not provided").Err()
 	e2 := ErrGRPCEmptyKey
 	e3 := ErrEmptyKey
 
 	if e1.Error() != e2.Error() {
 		t.Fatalf("expected %q == %q", e1.Error(), e2.Error())
 	}
-	if grpc.Code(e1) != e3.(EtcdError).Code() {
-		t.Fatalf("expected them to be equal, got %v / %v", grpc.Code(e1), e3.(EtcdError).Code())
+	ev1, _ := status.FromError(e1)
+	if ev1.Code() != e3.(EtcdError).Code() {
+		t.Fatalf("expected them to be equal, got %v / %v", ev1.Code(), e3.(EtcdError).Code())
 	}
 
 	if e1.Error() == e3.Error() {
 		t.Fatalf("expected %q != %q", e1.Error(), e3.Error())
 	}
-	if grpc.Code(e2) != e3.(EtcdError).Code() {
-		t.Fatalf("expected them to be equal, got %v / %v", grpc.Code(e2), e3.(EtcdError).Code())
+	ev2, _ := status.FromError(e2)
+	if ev2.Code() != e3.(EtcdError).Code() {
+		t.Fatalf("expected them to be equal, got %v / %v", ev2.Code(), e3.(EtcdError).Code())
 	}
 }
