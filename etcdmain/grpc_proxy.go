aa317e1a3a32ec5175acf097e1fd09bc1667108d
Peter Woodman <pwoodman@uber.com>
2017-09-13 12:33:29 -0400

    grpcproxy: don’t serve SSL when using upstream TLS auth

--- etcdmain/grpc_proxy.go
+++ etcdmain/grpc_proxy.go
@@ -15,7 +15,6 @@
 package etcdmain
 
 import (
-	"crypto/tls"
 	"fmt"
 	"net"
 	"net/http"
@@ -192,12 +191,7 @@ func startGRPCProxy(cmd *cobra.Command, args []string) {
 	}
 
 	var httpl net.Listener
-	if cfg.TLS != nil {
-		srvhttp.TLSConfig = cfg.TLS
-		httpl = tls.NewListener(m.Match(cmux.Any()), cfg.TLS)
-	} else {
-		httpl = m.Match(cmux.HTTP1())
-	}
+	httpl = m.Match(cmux.HTTP1())
 	go func() { errc <- srvhttp.Serve(httpl) }()
 
 	go func() { errc <- m.Serve() }()
