92f5746c54228ee3196ae9748eb0925c6c4e55d2
Gyu-Ho Lee <gyuhox@gmail.com>
2017-09-27 09:23:39 -0700

    clientv3/integration: fix license, minor nits in leasing_test.go
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- clientv3/integration/leasing_test.go
+++ clientv3/integration/leasing_test.go
@@ -3,7 +3,8 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// //     http://www.apache.org/licenses/LICENSE-2.0
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -1828,7 +1829,7 @@ func TestLeasingSessionExpire(t *testing.T) {
 
 	// down endpoint lkv uses for keepalives
 	clus.Members[0].Stop(t)
-	if err := waitForLeasingExpire(clus.Client(1), "foo/abc"); err != nil {
+	if err = waitForLeasingExpire(clus.Client(1), "foo/abc"); err != nil {
 		t.Fatal(err)
 	}
 	waitForExpireAck(t, lkv)
