d57159f79ab86aeb8682efc5b34a509c0602c4d4
Gyu-Ho Lee <gyuhox@gmail.com>
2017-10-05 14:58:25 -0700

    e2e/docker-dns-srv: use 'etcd.local' as SRV, clean up
    Signed-off-by: Gyu-Ho Lee <gyuhox@gmail.com>

--- e2e/docker-dns-srv/run.sh
+++ e2e/docker-dns-srv/run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 /etc/init.d/bind9 start
+
 # get rid of hosts so go lookup won't resolve 127.0.0.1 to localhost
 cat /dev/null >/etc/hosts
+
 goreman -f /Procfile start &
-sleep 5s
-./etcdctl --discovery-srv etcd-srv.local set foo bar
-ETCDCTL_API=3 ./etcdctl --discovery-srv etcd-srv.local put foo bar
+sleep 7s
+
+ETCDCTL_API=3 ./etcdctl \
+  --discovery-srv etcd.local \
+  put foo bar
