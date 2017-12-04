e18afc462b61ccefaf680052ecea2761a1adbe7a
Rene Zbinden <rene.zbinden@postfinance.ch>
2017-12-04 14:22:34 +0100

    clientv3: Fix comment for DialKeepAliveTime and DialKeepAliveTimeout

--- clientv3/config.go
+++ clientv3/config.go
@@ -33,11 +33,11 @@ type Config struct {
 	// DialTimeout is the timeout for failing to establish a connection.
 	DialTimeout time.Duration `json:"dial-timeout"`
 
-	// DialKeepAliveTime is the time in seconds after which client pings the server to see if
+	// DialKeepAliveTime is the time after which client pings the server to see if
 	// transport is alive.
 	DialKeepAliveTime time.Duration `json:"dial-keep-alive-time"`
 
-	// DialKeepAliveTimeout is the time in seconds that the client waits for a response for the
+	// DialKeepAliveTimeout is the time that the client waits for a response for the
 	// keep-alive probe.  If the response is not received in this time, the connection is closed.
 	DialKeepAliveTimeout time.Duration `json:"dial-keep-alive-timeout"`
 
