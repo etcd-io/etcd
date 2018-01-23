60d5ab548564d7512c0a629b0411507b807e0e3c
Gyuho Lee <gyuhox@gmail.com>
2018-01-23 15:54:52 -0800

    raftsnap: rename "snap" to "raftsnap"
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- raftsnap/snapshotter.go
+++ raftsnap/snapshotter.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package snap stores raft nodes' states with snapshots.
-package snap
+// Package raftsnap stores raft nodes' states with snapshots.
+package raftsnap
 
 import (
 	"errors"
@@ -30,7 +30,7 @@ import (
 	"github.com/coreos/etcd/pkg/pbutil"
 	"github.com/coreos/etcd/raft"
 	"github.com/coreos/etcd/raft/raftpb"
-	"github.com/coreos/etcd/snap/snappb"
+	"github.com/coreos/etcd/raftsnap/snappb"
 
 	"github.com/coreos/pkg/capnslog"
 )
