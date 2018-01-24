d095cc6ff07307fa93f640cc6afb87420d331d38
Gyuho Lee <gyuhox@gmail.com>
2018-01-24 10:28:06 -0800

    raftsnap: add "doc.go"
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- raftsnap/snapshotter.go
+++ raftsnap/snapshotter.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package raftsnap stores raft nodes' states with snapshots.
 package raftsnap
 
 import (
