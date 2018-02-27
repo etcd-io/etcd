e42c2b7bd2382b7ebc435e1586abebac243b7791
Gyuho Lee <gyuhox@gmail.com>
2018-02-26 17:29:57 -0800

    tools/etcd-test-proxy: godoc
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- tools/etcd-test-proxy/main.go
+++ tools/etcd-test-proxy/main.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// etcd-test-proxy is a proxy layer that simulates various network conditions.
 package main
 
 import (
