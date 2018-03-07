660f7fd8a081a08b2e6baf649436d4ae4d6c1408
Gyuho Lee <gyuhox@gmail.com>
2018-03-07 14:22:18 -0800

    hack/scripts-dev: sync with master
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- hack/benchmark/bench.sh
+++ hack/benchmark/bench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-leader=http://10.240.201.15:2379
+leader=http://localhost:2379
 # assume three servers
-servers=( http://10.240.201.15:2379 http://10.240.212.209:2379 http://10.240.95.3:2379 )
+servers=( http://localhost:2379 http://localhost:22379 http://localhost:32379 )
 
 keyarray=( 64 256 )
 
