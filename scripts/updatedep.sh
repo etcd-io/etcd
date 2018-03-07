27f0aa9cf0fcb8961ed6f7114a3ecbc6396dbf3a
Gyuho Lee <gyuhox@gmail.com>
2018-03-07 12:47:43 -0800

    scripts: document "updatedep.sh" script
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- scripts/updatedep.sh
+++ scripts/updatedep.sh
@@ -7,5 +7,14 @@ if ! [[ "$0" =~ scripts/updatedep.sh ]]; then
 fi
 
 go get -v -u github.com/golang/dep/cmd/dep
-dep ensure -v
+
+if [[ -z "$1" ]]; then
+  echo "dep ensure on all packages"
+  dep ensure -v
+else
+  echo "dep update on" "$1"
+  # shellcheck disable=SC2086
+  dep ensure -v -update $1
+fi
+
 dep prune
