ae56cf014de9ada070e8a5408c4b28132f36b3b5
Gyuho Lee <gyuhox@gmail.com>
2018-03-30 11:46:03 -0700

    functional-tester/rpcpb: simplify "return err"
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- tools/functional-tester/rpcpb/member.go
+++ tools/functional-tester/rpcpb/member.go
@@ -86,10 +86,7 @@ func (m *Member) Defrag() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	_, err = cli.Defragment(ctx, m.EtcdClientEndpoint)
 	cancel()
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
 // RevHash fetches current revision and hash on this member.
