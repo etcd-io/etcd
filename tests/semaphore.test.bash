774f6652df1045c1ffd5c42f56c6cc05833a8000
Gyuho Lee <gyuhox@gmail.com>
2018-04-09 10:48:54 -0700

    semaphore.sh: move to "tests/semaphore.test.bash"
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

---
+++ tests/semaphore.test.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if ! [[ "$0" =~ "tests/semaphore.test.bash" ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+TEST_SUFFIX=$(date +%s | base64 | head -c 15)
+
+TEST_OPTS="PASSES='build unit release integration_e2e functional' MANUAL_VER=v3.3.3"
+if [ "$TEST_ARCH" == "386" ]; then
+  TEST_OPTS="GOARCH=386 PASSES='build unit integration_e2e'"
+fi
+
+docker run \
+  --rm \
+  --volume=`pwd`:/go/src/github.com/coreos/etcd \
+  gcr.io/etcd-development/etcd-test:go1.10.1 \
+  /bin/bash -c "${TEST_OPTS} ./test 2>&1 | tee test-${TEST_SUFFIX}.log"
+
+! egrep "(--- FAIL:|panic: test timed out|appears to have leaked)" -B50 -A10 test-${TEST_SUFFIX}.log
