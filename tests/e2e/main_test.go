a8a5176f1d2b57029202a73b7d0259495ca7aab6
Gyuho Lee <gyuhox@gmail.com>
2018-04-09 11:12:42 -0700

    tests/e2e: fix exec paths
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- tests/e2e/main_test.go
+++ tests/e2e/main_test.go
@@ -33,8 +33,8 @@ func TestMain(m *testing.M) {
 	os.Setenv("ETCD_UNSUPPORTED_ARCH", runtime.GOARCH)
 	os.Unsetenv("ETCDCTL_API")
 
-	flag.StringVar(&binDir, "bin-dir", "../bin", "The directory for store etcd and etcdctl binaries.")
-	flag.StringVar(&certDir, "cert-dir", "../integration/fixtures", "The directory for store certificate files.")
+	flag.StringVar(&binDir, "bin-dir", "../../bin", "The directory for store etcd and etcdctl binaries.")
+	flag.StringVar(&certDir, "cert-dir", "../../integration/fixtures", "The directory for store certificate files.")
 	flag.Parse()
 
 	binPath = binDir + "/etcd"
