3a7b5c1b2b6ab55fdf1fbf01c547969b84ee13d9
Gyuho Lee <gyuhox@gmail.com>
2018-04-10 13:47:07 -0700

    clientv3/integration: log test failures from slow balancer as TODO
    Balancer tests have been flaky since last balancer rewrite.
    Should be from slow balancer failover. Once we re-rewrite balancer
    with roundrobin, there should be no more flaky tests as below:
    
    TestBalancerUnderServerStopInflightLinearizableGetOnRestart (27.62s)
    server_shutdown_test.go:343: context deadline exceeded
    
    TestBalancerUnderServerShutdownDelete (3.22s)
    util.go:32: context deadline exceeded
    
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- clientv3/integration/util.go
+++ clientv3/integration/util.go
@@ -25,7 +25,8 @@ import (
 // mustWaitPinReady waits up to 3-second until connection is up (pin endpoint).
 // Fatal on time-out.
 func mustWaitPinReady(t *testing.T, cli *clientv3.Client) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// TODO: decrease timeout after balancer rewrite!!!
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	_, err := cli.Get(ctx, "foo")
 	cancel()
 	if err != nil {
