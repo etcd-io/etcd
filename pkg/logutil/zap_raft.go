6df3179c06b2cd0c17a504dbdf3ff7d32b3b153c
Gyuho Lee <gyuhox@gmail.com>
2018-04-24 15:26:16 -0700

    pkg/logutil: add "NewRaftLoggerFromZapCore"
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- pkg/logutil/zap_raft.go
+++ pkg/logutil/zap_raft.go
@@ -1,8 +1,24 @@
+// Copyright 2018 The etcd Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package logutil
 
 import (
 	"github.com/coreos/etcd/raft"
+
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // NewRaftLogger converts "*zap.Logger" to "raft.Logger".
@@ -12,6 +28,14 @@ func NewRaftLogger(lcfg zap.Config) (raft.Logger, error) {
 		return nil, err
 	}
 	return &zapRaftLogger{lg: lg, sugar: lg.Sugar()}, nil
+}
+
+// NewRaftLoggerFromZapCore creates "raft.Logger" from "zap.Core"
+// and "zapcore.WriteSyncer".
+func NewRaftLoggerFromZapCore(cr zapcore.Core, syncer zapcore.WriteSyncer) raft.Logger {
+	// "AddCallerSkip" to annotate caller outside of "logutil"
+	lg := zap.New(cr, zap.AddCaller(), zap.AddCallerSkip(1), zap.ErrorOutput(syncer))
+	return &zapRaftLogger{lg: lg, sugar: lg.Sugar()}
 }
 
 type zapRaftLogger struct {
--- pkg/logutil/zap_raft_test.go
+++ pkg/logutil/zap_raft_test.go
@@ -20,10 +20,12 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func TestNewRaftLogger(t *testing.T) {
@@ -66,5 +68,22 @@ func TestNewRaftLogger(t *testing.T) {
 	}
 	if !bytes.Contains(data, []byte("logutil/zap_raft_test.go:")) {
 		t.Fatalf("unexpected caller; %q", string(data))
+	}
+}
+
+func TestNewRaftLoggerFromZapCore(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	syncer := zapcore.AddSync(buf)
+	cr := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		syncer,
+		zap.NewAtomicLevelAt(zap.InfoLevel),
+	)
+
+	lg := NewRaftLoggerFromZapCore(cr, syncer)
+	lg.Info("TestNewRaftLoggerFromZapCore")
+	txt := buf.String()
+	if !strings.Contains(txt, "TestNewRaftLoggerFromZapCore") {
+		t.Fatalf("unexpected log %q", txt)
 	}
 }
