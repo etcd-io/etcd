fcbb30364a9b2afc5d14571a28d4a6ec9c171372
Gyuho Lee <gyuhox@gmail.com>
2018-04-24 11:59:49 -0700

    pkg/logutil: add "NewJournaldWriter"
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- pkg/logutil/zap_grpc.go
+++ pkg/logutil/zap_grpc.go
@@ -15,8 +15,6 @@
 package logutil
 
 import (
-	"github.com/coreos/etcd/raft"
-
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc/grpclog"
@@ -101,66 +99,4 @@ func (zl *zapGRPCLogger) V(l int) bool {
 		return !zl.lg.Core().Enabled(zapcore.DebugLevel)
 	}
 	return true
-}
-
-// NewRaftLogger converts "*zap.Logger" to "raft.Logger".
-func NewRaftLogger(lcfg zap.Config) (raft.Logger, error) {
-	lg, err := lcfg.Build(zap.AddCallerSkip(1)) // to annotate caller outside of "logutil"
-	if err != nil {
-		return nil, err
-	}
-	return &zapRaftLogger{lg: lg, sugar: lg.Sugar()}, nil
-}
-
-type zapRaftLogger struct {
-	lg    *zap.Logger
-	sugar *zap.SugaredLogger
-}
-
-func (zl *zapRaftLogger) Debug(args ...interface{}) {
-	zl.sugar.Debug(args...)
-}
-
-func (zl *zapRaftLogger) Debugf(format string, args ...interface{}) {
-	zl.sugar.Debugf(format, args...)
-}
-
-func (zl *zapRaftLogger) Error(args ...interface{}) {
-	zl.sugar.Error(args...)
-}
-
-func (zl *zapRaftLogger) Errorf(format string, args ...interface{}) {
-	zl.sugar.Errorf(format, args...)
-}
-
-func (zl *zapRaftLogger) Info(args ...interface{}) {
-	zl.sugar.Info(args...)
-}
-
-func (zl *zapRaftLogger) Infof(format string, args ...interface{}) {
-	zl.sugar.Infof(format, args...)
-}
-
-func (zl *zapRaftLogger) Warning(args ...interface{}) {
-	zl.sugar.Warn(args...)
-}
-
-func (zl *zapRaftLogger) Warningf(format string, args ...interface{}) {
-	zl.sugar.Warnf(format, args...)
-}
-
-func (zl *zapRaftLogger) Fatal(args ...interface{}) {
-	zl.sugar.Fatal(args...)
-}
-
-func (zl *zapRaftLogger) Fatalf(format string, args ...interface{}) {
-	zl.sugar.Fatalf(format, args...)
-}
-
-func (zl *zapRaftLogger) Panic(args ...interface{}) {
-	zl.sugar.Panic(args...)
-}
-
-func (zl *zapRaftLogger) Panicf(format string, args ...interface{}) {
-	zl.sugar.Panicf(format, args...)
 }
--- pkg/logutil/zap_grpc_test.go
+++ pkg/logutil/zap_grpc_test.go
@@ -66,50 +66,7 @@ func TestNewGRPCLoggerV2(t *testing.T) {
 	if !bytes.Contains(data, []byte("etcd-logutil-2")) {
 		t.Fatalf("can't find data in log %q", string(data))
 	}
-	if !bytes.Contains(data, []byte("logutil/zap_test.go:")) {
-		t.Fatalf("unexpected caller; %q", string(data))
-	}
-}
-
-func TestNewRaftLogger(t *testing.T) {
-	logPath := filepath.Join(os.TempDir(), fmt.Sprintf("test-log-%d", time.Now().UnixNano()))
-	defer os.RemoveAll(logPath)
-
-	lcfg := zap.Config{
-		Level:       zap.NewAtomicLevelAt(zap.DebugLevel),
-		Development: false,
-		Sampling: &zap.SamplingConfig{
-			Initial:    100,
-			Thereafter: 100,
-		},
-		Encoding:         "json",
-		EncoderConfig:    zap.NewProductionEncoderConfig(),
-		OutputPaths:      []string{logPath},
-		ErrorOutputPaths: []string{logPath},
-	}
-	gl, err := NewRaftLogger(lcfg)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	gl.Info("etcd-logutil-1")
-	data, err := ioutil.ReadFile(logPath)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Contains(data, []byte("etcd-logutil-1")) {
-		t.Fatalf("can't find data in log %q", string(data))
-	}
-
-	gl.Warning("etcd-logutil-2")
-	data, err = ioutil.ReadFile(logPath)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !bytes.Contains(data, []byte("etcd-logutil-2")) {
-		t.Fatalf("can't find data in log %q", string(data))
-	}
-	if !bytes.Contains(data, []byte("logutil/zap_test.go:")) {
+	if !bytes.Contains(data, []byte("logutil/zap_grpc_test.go:")) {
 		t.Fatalf("unexpected caller; %q", string(data))
 	}
 }
