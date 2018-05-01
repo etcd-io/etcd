f6a14fb72cb54825592a3090ad7c3dee0c26ef82
Gyuho Lee <gyuhox@gmail.com>
2018-05-01 15:28:25 -0700

    clientv3: use "zap" logger in integration tests
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- clientv3/integration/logger_test.go
+++ clientv3/integration/logger_test.go
@@ -19,11 +19,9 @@ import (
 
 	"github.com/coreos/etcd/clientv3"
 
-	"github.com/coreos/pkg/capnslog"
 	"google.golang.org/grpc/grpclog"
 )
 
 func init() {
-	capnslog.SetGlobalLogLevel(capnslog.CRITICAL)
 	clientv3.SetLogger(grpclog.NewLoggerV2(ioutil.Discard, ioutil.Discard, ioutil.Discard))
 }
