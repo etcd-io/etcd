5b0683cd0839fd8fc79b5a398ad6c5a803ac71ad
Gyuho Lee <gyuhox@gmail.com>
2018-05-02 14:42:55 -0700

    tests/e2e: investigating TestCtlV3AuthAndWatch failure in coverage tests
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- tests/e2e/ctl_v3_auth_test.go
+++ tests/e2e/ctl_v3_auth_test.go
@@ -40,7 +40,14 @@ func TestCtlV3AuthCertCN(t *testing.T)           { testCtl(t, authTestCertCN, wi
 func TestCtlV3AuthRevokeWithDelete(t *testing.T) { testCtl(t, authTestRevokeWithDelete) }
 func TestCtlV3AuthInvalidMgmt(t *testing.T)      { testCtl(t, authTestInvalidMgmt) }
 func TestCtlV3AuthFromKeyPerm(t *testing.T)      { testCtl(t, authTestFromKeyPerm) }
-func TestCtlV3AuthAndWatch(t *testing.T)         { testCtl(t, authTestWatch) }
+
+func TestCtlV3AuthAndWatch(t *testing.T) {
+	oldenv := os.Getenv("EXPECT_DEBUG")
+	defer os.Setenv("EXPECT_DEBUG", oldenv)
+	os.Setenv("EXPECT_DEBUG", "1")
+
+	testCtl(t, authTestWatch)
+}
 
 func TestCtlV3AuthLeaseTestKeepAlive(t *testing.T)         { testCtl(t, authLeaseTestKeepAlive) }
 func TestCtlV3AuthLeaseTestTimeToLiveExpired(t *testing.T) { testCtl(t, authLeaseTestTimeToLiveExpired) }
