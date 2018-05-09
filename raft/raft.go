2302fe984fc47a24198c3c6e8a6c0e88adbdaf13
qupeng <qupeng@pingcap.com>
2018-05-09 13:10:39 +0800

    New created peers should accept any snapshots.

--- raft/raft.go
+++ raft/raft.go
@@ -1270,7 +1270,10 @@ func (r *raft) restore(s pb.Snapshot) bool {
 	}
 
 	// The normal peer can't become learner.
-	if !r.isLearner {
+	//
+	// Both of `prs` and `learnerPrs` are empty means the peer is new created by
+	// conf change, in which case we should accept snapshots make it as learner.
+	if (r.prs.len() > 0 || r.learnerPrs.len() > 0) && !r.isLearner {
 		for _, id := range s.Metadata.ConfState.Learners {
 			if id == r.id {
 				r.logger.Errorf("%x can't become learner when restores snapshot [index: %d, term: %d]", r.id, s.Metadata.Index, s.Metadata.Term)
