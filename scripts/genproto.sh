d29a0c7172a06d353e4e4056bfc32587f5807233
Gyuho Lee <gyuhox@gmail.com>
2018-05-22 13:42:43 -0700

    scripts/genproto: use latest "gogo/protobuf"
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -19,7 +19,7 @@ fi
 DIRS="./wal/walpb ./etcdserver/etcdserverpb ./etcdserver/api/snap/snappb ./raft/raftpb ./mvcc/mvccpb ./lease/leasepb ./auth/authpb ./etcdserver/api/v3lock/v3lockpb ./etcdserver/api/v3election/v3electionpb"
 
 # exact version of packages to build
-GOGO_PROTO_SHA="41168f6614b7bb144818ec8967b8c702705df564"
+GOGO_PROTO_SHA="1adfc126b41513cc696b209667c8656ea7aac67c"
 GRPC_GATEWAY_SHA="61c34cc7e0c7a0d85e4237d665e622640279ff3d"
 SCHWAG_SHA="b7d0fc9aadaaae3d61aaadfc12e4a2f945514912"
 
