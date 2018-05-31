29121a8ceeee6a26addbce000e6068b94e01ca8b
Wenjia Zhang <wenjiazhang@google.com>
2018-05-31 13:45:37 -0700

    etcd-dump-logs: add decoder support

---
+++ tools/etcd-dump-logs/testdecoder/decoder_correctoutputformat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while read line
+do
+  LEN=$(echo ${#line})
+  if [ $LEN -ge 20 ]; then
+  	echo "OK|$line" | tr 1234567890 abcdefghij
+  else
+  	echo "ERROR|$line" | tr 1234567890 abcdefghij
+  fi
+done < "${1:-/dev/stdin}"
