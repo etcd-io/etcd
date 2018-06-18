339cda03b31e0d0c857f14fa1c133f865d04025d
Gyuho Lee <gyuhox@gmail.com>
2018-06-18 09:41:37 -0700

    etcdserver/api/v3rpc: remove duplicate gRPC logger set
    Fix
    
    === RUN   TestEmbedEtcd
    ==================
    WARNING: DATA RACE
    Write at 0x000001df86d0 by goroutine 711:
      github.com/coreos/etcd/embed.(*Config).setupLogging.func1()
          /go/src/github.com/coreos/etcd/vendor/google.golang.org/grpc/grpclog/loggerv2.go:68 +0x16c
      sync.(*Once).Do()
          /usr/local/go/src/sync/once.go:44 +0xe1
      github.com/coreos/etcd/embed.(*Config).setupLogging()
    
    in gRPC proxy tests.
    
    Signed-off-by: Gyuho Lee <gyuhox@gmail.com>

--- etcdserver/api/v3rpc/grpc.go
+++ etcdserver/api/v3rpc/grpc.go
@@ -16,10 +16,7 @@ package v3rpc
 
 import (
 	"crypto/tls"
-	"io/ioutil"
 	"math"
-	"os"
-	"sync"
 
 	"github.com/coreos/etcd/etcdserver"
 	pb "github.com/coreos/etcd/etcdserver/etcdserverpb"
@@ -27,7 +24,6 @@ import (
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
@@ -37,9 +33,6 @@ const (
 	maxStreams        = math.MaxUint32
 	maxSendBytes      = math.MaxInt32
 )
-
-// integration tests call this multiple times, which is racey in gRPC side
-var grpclogOnce sync.Once
 
 func Server(s *etcdserver.EtcdServer, tls *tls.Config, gopts ...grpc.ServerOption) *grpc.Server {
 	var opts []grpc.ServerOption
@@ -70,17 +63,6 @@ func Server(s *etcdserver.EtcdServer, tls *tls.Config, gopts ...grpc.ServerOptio
 
 	// set zero values for metrics registered for this grpc server
 	grpc_prometheus.Register(grpcServer)
-
-	grpclogOnce.Do(func() {
-		if s.Cfg.Debug {
-			grpc.EnableTracing = true
-			// enable info, warning, error
-			grpclog.SetLoggerV2(grpclog.NewLoggerV2(os.Stderr, os.Stderr, os.Stderr))
-		} else {
-			// only discard info
-			grpclog.SetLoggerV2(grpclog.NewLoggerV2(ioutil.Discard, os.Stderr, os.Stderr))
-		}
-	})
 
 	return grpcServer
 }
