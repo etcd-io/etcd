a421a604d6a8c059b4d839e71c35c3feb7c80d42
Wenjia <wenjiazhang@google.com>
2018-07-20 13:49:58 -0700

    remove hashRevDurations

--- mvcc/metrics.go
+++ mvcc/metrics.go
@@ -208,7 +208,6 @@ func init() {
 	prometheus.MustRegister(dbTotalSize)
 	prometheus.MustRegister(dbTotalSizeInUse)
 	prometheus.MustRegister(hashDurations)
-	prometheus.MustRegister(hashRevDurations)
 }
 
 // ReportEventReceived reports that an event is received.
