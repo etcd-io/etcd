d1acb5a5c8ad0a7ee6a16388fef1d0a353ebff3b
Gyuho Lee <leegyuho@amazon.com>
2018-08-29 14:49:01 -0700

    etcdserver: add "etcd_server_id"
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- etcdserver/server.go
+++ etcdserver/server.go
@@ -59,6 +59,8 @@ import (
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/coreos/pkg/capnslog"
+	"github.com/prometheus/client_golang/prometheus"
+	"golang.org/x/net/context"
 )
 
 const (
@@ -435,6 +437,7 @@ func NewServer(cfg ServerConfig) (srv *EtcdServer, err error) {
 		reqIDGen:      idutil.NewGenerator(uint16(id), time.Now()),
 		forceVersionC: make(chan struct{}),
 	}
+	serverID.With(prometheus.Labels{"server_id": id.String()}).Set(1)
 
 	srv.applyV2 = &applierV2store{store: srv.store, cluster: srv.cluster}
 
