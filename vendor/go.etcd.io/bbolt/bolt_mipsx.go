fa57f7fbc787b4ab424502093dc599d0b21be275
Gyuho Lee <leegyuho@amazon.com>
2018-08-29 12:39:59 -0700

    vendor: use "go.etcd.io/bbolt" v1.3.1-etcd.7
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- vendor/go.etcd.io/bbolt/bolt_mipsx.go
+++ vendor/go.etcd.io/bbolt/bolt_mipsx.go
@@ -1,12 +1,12 @@
-// +build mips64 mips64le
+// +build mips mipsle
 
-package bolt
+package bbolt
 
 // maxMapSize represents the largest mmap size supported by Bolt.
-const maxMapSize = 0x8000000000 // 512GB
+const maxMapSize = 0x40000000 // 1GB
 
 // maxAllocSize is the size used when creating array pointers.
-const maxAllocSize = 0x7FFFFFFF
+const maxAllocSize = 0xFFFFFFF
 
 // Are unaligned load/stores broken on this arch?
 var brokenUnaligned = false
