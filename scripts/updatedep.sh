d5967b40db6c35974249773094ac03c4d0b09b36
Gyuho Lee <leegyuho@amazon.com>
2018-09-21 01:59:09 -0700

    scripts/updatedep: fix shellcheck errors
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- scripts/updatedep.sh
+++ scripts/updatedep.sh
@@ -7,13 +7,9 @@ if ! [[ "$0" =~ scripts/updatedep.sh ]]; then
 fi
 
 if [[ $(go version) != "go version go1.11"* ]]; then
-  echo "expect Go 1.11+, got:" $(go version)
+  echo "expect Go 1.11+, got:" "$(go version)"
   exit 255
 fi
-
-<<COMMENT
-GO111MODULE=on go mod init
-COMMENT
 
 GO111MODULE=on go mod tidy -v
 GO111MODULE=on go mod vendor -v
