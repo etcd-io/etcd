a038dc6464f2e4f79bcd5ae0af22907408b8d69f
Gyuho Lee <leegyuho@amazon.com>
2018-11-13 21:48:38 -0800

    tests/e2e: test cluster version
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- e2e/etcd_release_upgrade_test.go
+++ e2e/etcd_release_upgrade_test.go
@@ -101,6 +101,15 @@ func TestReleaseUpgrade(t *testing.T) {
 			}
 		}
 	}
+
+	// expect upgraded cluster version
+	ver := version.Version
+	if strings.HasSuffix(ver, "+git") {
+		ver = strings.Replace(ver, "+git", "", 1)
+	}
+	if err := cURLGet(cx.epc, cURLReq{endpoint: "/metrics", expected: fmt.Sprintf(`etcd_cluster_version{cluster_version="%s"} 1`, ver), metricsURLScheme: cx.cfg.metricsURLScheme}); err != nil {
+		cx.t.Fatalf("failed get with curl (%v)", err)
+	}
 }
 
 func TestReleaseUpgradeWithRestart(t *testing.T) {
