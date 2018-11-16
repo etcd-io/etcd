f8a513ce65d42612acda9e1e4c4fd5afe1b77e56
Gyuho Lee <leegyuho@amazon.com>
2018-11-15 22:35:41 -0800

    tools/etcd-dump-db: add "--timeout" flag
    By default, wait up to 10 seconds to obtain flocks.
    
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- tools/etcd-dump-db/backend.go
+++ tools/etcd-dump-db/backend.go
@@ -32,9 +32,9 @@ func snapDir(dataDir string) string {
 }
 
 func getBuckets(dbPath string) (buckets []string, err error) {
-	db, derr := bolt.Open(dbPath, 0600, &bolt.Options{})
+	db, derr := bolt.Open(dbPath, 0600, &bolt.Options{Timeout: flockTimeout})
 	if derr != nil {
-		return nil, derr
+		return nil, fmt.Errorf("failed to open bolt DB %v", derr)
 	}
 	defer db.Close()
 
@@ -94,9 +94,9 @@ func leaseDecoder(k, v []byte) {
 }
 
 func iterateBucket(dbPath, bucket string, limit uint64, decode bool) (err error) {
-	db, derr := bolt.Open(dbPath, 0600, &bolt.Options{})
-	if derr != nil {
-		return derr
+	db, err := bolt.Open(dbPath, 0600, &bolt.Options{Timeout: flockTimeout})
+	if err != nil {
+		return fmt.Errorf("failed to open bolt DB %v", err)
 	}
 	defer db.Close()
 
