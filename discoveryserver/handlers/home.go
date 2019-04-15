9199cd046afed17a9fcf4a54e966a739945dcaa5
Brandon Philips <brandon.philips@coreos.com>
2019-04-15 17:43:13 +0000

    discoveryserver: set home to github docs
    The CoreOS docs are no longer maintained. Point the home to the github
    docs on how to use the discovery service.

--- discoveryserver/handlers/home.go
+++ discoveryserver/handlers/home.go
@@ -5,9 +5,8 @@ import (
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-
 	http.Redirect(w, r,
-		"https://coreos.com/docs/cluster-management/setup/cluster-discovery/",
+		"https://github.com/etcd-io/etcd/blob/master/Documentation/v2/clustering.md#public-etcd-discovery-service",
 		http.StatusMovedPermanently,
 	)
 }
