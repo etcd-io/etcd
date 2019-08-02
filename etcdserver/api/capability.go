7fbbb9c8bf840a09121ecc34a1c61c637ccbafe3
Gyuho Lee <leegyuho@amazon.com>
2019-08-02 15:27:54 -0700

    *: add 3.5 capability for 3.5 dev tree
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- etcdserver/api/capability.go
+++ etcdserver/api/capability.go
@@ -41,6 +41,7 @@ var (
 		"3.2.0": {AuthCapability: true, V3rpcCapability: true},
 		"3.3.0": {AuthCapability: true, V3rpcCapability: true},
 		"3.4.0": {AuthCapability: true, V3rpcCapability: true},
+		"3.5.0": {AuthCapability: true, V3rpcCapability: true},
 	}
 
 	enableMapMu sync.RWMutex
