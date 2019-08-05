4b0af5b4ac160e66cd7af5ef93e94e9d4498b41a
Gyuho Lee <leegyuho@amazon.com>
2019-08-04 23:52:17 -0700

    clientv3: document "WithBlock" dial option
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- clientv3/config.go
+++ clientv3/config.go
@@ -68,6 +68,8 @@ type Config struct {
 	RejectOldCluster bool `json:"reject-old-cluster"`
 
 	// DialOptions is a list of dial options for the grpc client (e.g., for interceptors).
+	// For example, pass "grpc.WithBlock()" to block until the underlying connection is up.
+	// Without this, Dial returns immediately and connecting the server happens in background.
 	DialOptions []grpc.DialOption
 
 	// Context is the default client context; it can be used to cancel grpc dial out and
