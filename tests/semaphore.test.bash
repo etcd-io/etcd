92a750432faa6bbafa5a8b817fc26dba6538d662
Gyuho Lee <leegyuho@amazon.com>
2019-08-14 01:19:41 -0700

    tests: update
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- tests/semaphore.test.bash
+++ tests/semaphore.test.bash
@@ -8,10 +8,10 @@ fi
 <<COMMENT
 # amd64-e2e
 tests/semaphore.test.bash
-sudo HOST_TMP_DIR=/tmp TEST_OPTS="PASSES='build release e2e' MANUAL_VER=v3.3.7" make docker-test
+sudo HOST_TMP_DIR=/tmp TEST_OPTS="PASSES='build release e2e' MANUAL_VER=v3.3.13" make docker-test
 
 # 386-e2e
 sudo HOST_TMP_DIR=/tmp TEST_OPTS="GOARCH=386 PASSES='build e2e'" make docker-test
 COMMENT
 
-sudo HOST_TMP_DIR=/tmp TEST_OPTS="PASSES='build release e2e' MANUAL_VER=v3.3.7" make docker-test
+sudo HOST_TMP_DIR=/tmp TEST_OPTS="PASSES='build release e2e' MANUAL_VER=v3.3.13" make docker-test
