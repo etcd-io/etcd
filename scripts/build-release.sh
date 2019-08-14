d7fc66bcbb2a3096546a93473305cfd09ea6ae26
Gyuho Lee <leegyuho@amazon.com>
2019-08-14 01:14:34 -0700

    scripts: update release, genproto, dep
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -11,11 +11,6 @@ if [ -z "${VERSION}" ]; then
 	exit 255
 fi
 
-if ! command -v acbuild >/dev/null; then
-    echo "cannot find acbuild"
-    exit 1
-fi
-
 if ! command -v docker >/dev/null; then
     echo "cannot find docker"
     exit 1
@@ -27,14 +22,8 @@ pushd "${ETCD_ROOT}" >/dev/null
 	echo Building etcd binary...
 	./scripts/build-binary "${VERSION}"
 
-	# ppc64le not yet supported by acbuild.
-	for TARGET_ARCH in "amd64" "arm64"; do
-		echo Building ${TARGET_ARCH} aci image...
-		GOARCH=${TARGET_ARCH} BINARYDIR=release/etcd-${VERSION}-linux-${TARGET_ARCH} BUILDDIR=release ./scripts/build-aci "${VERSION}"
-	done
-
 	for TARGET_ARCH in "amd64" "arm64" "ppc64le"; do
 		echo Building ${TARGET_ARCH} docker image...
-		GOARCH=${TARGET_ARCH} BINARYDIR=release/etcd-${VERSION}-linux-${TARGET_ARCH} BUILDDIR=release ./scripts/build-docker "${VERSION}"
+		GOOS=linux GOARCH=${TARGET_ARCH} BINARYDIR=release/etcd-${VERSION}-linux-${TARGET_ARCH} BUILDDIR=release ./scripts/build-docker "${VERSION}"
 	done
 popd >/dev/null
--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -10,17 +10,20 @@ if ! [[ "$0" =~ scripts/genproto.sh ]]; then
 	exit 255
 fi
 
-if [[ $(protoc --version | cut -f2 -d' ') != "3.5.0" ]]; then
-	echo "could not find protoc 3.5.0, is it installed + in PATH?"
+if [[ $(protoc --version | cut -f2 -d' ') != "3.7.1" ]]; then
+	echo "could not find protoc 3.7.1, is it installed + in PATH?"
 	exit 255
 fi
 
 # directories containing protos to be built
 DIRS="./wal/walpb ./etcdserver/etcdserverpb ./snap/snappb ./raft/raftpb ./mvcc/mvccpb ./lease/leasepb ./auth/authpb ./etcdserver/api/v3lock/v3lockpb ./etcdserver/api/v3election/v3electionpb"
 
+# disable go mod
+export GO111MODULE=off
+
 # exact version of packages to build
-GOGO_PROTO_SHA="41168f6614b7bb144818ec8967b8c702705df564"
-GRPC_GATEWAY_SHA="a92d37fb6339375fa4bb7d9c364f92373fe199c3"
+GOGO_PROTO_SHA="1adfc126b41513cc696b209667c8656ea7aac67c"
+GRPC_GATEWAY_SHA="92583770e3f01b09a0d3e9bdf64321d8bebd48f2"
 SCHWAG_SHA="b7d0fc9aadaaae3d61aaadfc12e4a2f945514912"
 
 # set up self-contained GOPATH for building
@@ -28,39 +31,57 @@ export GOPATH=${PWD}/gopath.proto
 export GOBIN=${PWD}/bin
 export PATH="${GOBIN}:${PATH}"
 
-COREOS_ROOT="${GOPATH}/src/github.com/coreos"
-ETCD_ROOT="${COREOS_ROOT}/etcd"
+ETCD_IO_ROOT="${GOPATH}/src/github.com/coreos/etcd"
+ETCD_ROOT="${ETCD_IO_ROOT}/etcd"
 GOGOPROTO_ROOT="${GOPATH}/src/github.com/gogo/protobuf"
 SCHWAG_ROOT="${GOPATH}/src/github.com/hexfusion/schwag"
 GOGOPROTO_PATH="${GOGOPROTO_ROOT}:${GOGOPROTO_ROOT}/protobuf"
 GRPC_GATEWAY_ROOT="${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway"
 
-rm -f "${ETCD_ROOT}"
-mkdir -p "${COREOS_ROOT}"
+function cleanup {
+  # Remove the whole fake GOPATH which can really confuse go mod.
+  rm -rf "${PWD}/gopath.proto"
+}
+
+cleanup
+trap cleanup EXIT
+
+mkdir -p "${ETCD_IO_ROOT}"
 ln -s "${PWD}" "${ETCD_ROOT}"
 
-# Ensure we have the right version of protoc-gen-gogo by building it every time.
-# TODO(jonboulle): vendor this instead of `go get`ting it.
-go get -u github.com/gogo/protobuf/{proto,protoc-gen-gogo,gogoproto}
-go get -u golang.org/x/tools/cmd/goimports
+echo "Installing gogo/protobuf..."
+GOGOPROTO_ROOT="$GOPATH/src/github.com/gogo/protobuf"
+# rm -rf $GOGOPROTO_ROOT
+mkdir -p $GOPATH/src/github.com/gogo
+pushd $GOPATH/src/github.com/gogo
+  git clone https://github.com/gogo/protobuf.git
+popd
 pushd "${GOGOPROTO_ROOT}"
-	git reset --hard "${GOGO_PROTO_SHA}"
-	make install
+  git reset --hard HEAD
+  make install
 popd
 
-# generate gateway code
-go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+echo "Installing grpc-ecosystem/grpc-gateway..."
+GRPC_GATEWAY_ROOT="$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway"
+# rm -rf $GRPC_GATEWAY_ROOT
+mkdir -p $GOPATH/src/github.com/grpc-ecosystem
+go get -v -d github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -v -d github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+pushd $GOPATH/src/github.com/grpc-ecosystem
+  rm -rf ./grpc-gateway
+  git clone https://github.com/grpc-ecosystem/grpc-gateway.git
+popd
 pushd "${GRPC_GATEWAY_ROOT}"
 	git reset --hard "${GRPC_GATEWAY_SHA}"
-	go install ./protoc-gen-grpc-gateway
+	go install -v ./protoc-gen-grpc-gateway
+	go install -v ./protoc-gen-swagger
 popd
 
 for dir in ${DIRS}; do
 	pushd "${dir}"
-		protoc --gofast_out=plugins=grpc,import_prefix=github.com/coreos/:. -I=".:${GOGOPROTO_PATH}:${COREOS_ROOT}:${GRPC_GATEWAY_ROOT}/third_party/googleapis" ./*.proto
+		protoc --gofast_out=plugins=grpc,import_prefix=github.com/coreos/:. -I=".:${GOGOPROTO_PATH}:${ETCD_IO_ROOT}:${GRPC_GATEWAY_ROOT}/third_party/googleapis" ./*.proto
 		sed -i.bak -E "s/github\.com\/coreos\/(gogoproto|github\.com|golang\.org|google\.golang\.org)/\1/g" ./*.pb.go
-		sed -i.bak -E 's/github\.com\/coreos\/(errors|fmt|io)/\1/g' ./*.pb.go
+		sed -i.bak -E 's/github\.com\/coreos\/(errors|fmt|io|context|math\/bits)/\1/g' ./*.pb.go
 		sed -i.bak -E 's/import _ \"gogoproto\"//g' ./*.pb.go
 		sed -i.bak -E 's/import fmt \"fmt\"//g' ./*.pb.go
 		sed -i.bak -E 's/import _ \"github\.com\/coreos\/google\/api\"//g' ./*.pb.go
@@ -69,77 +90,3 @@ for dir in ${DIRS}; do
 		goimports -w ./*.pb.go
 	popd
 done
-
-# remove old swagger files so it's obvious whether the files fail to generate
-rm -rf Documentation/dev-guide/apispec/swagger/*json
-for pb in etcdserverpb/rpc api/v3lock/v3lockpb/v3lock api/v3election/v3electionpb/v3election; do
-	protobase="etcdserver/${pb}"
-	protoc -I. \
-	    -I"${GRPC_GATEWAY_ROOT}"/third_party/googleapis \
-	    -I"${GOGOPROTO_PATH}" \
-	    -I"${COREOS_ROOT}" \
-	    --grpc-gateway_out=logtostderr=true:. \
-	    --swagger_out=logtostderr=true:./Documentation/dev-guide/apispec/swagger/. \
-	    ${protobase}.proto
-	# hack to move gw files around so client won't include them
-	pkgpath=$(dirname "${protobase}")
-	pkg=$(basename "${pkgpath}")
-	gwfile="${protobase}.pb.gw.go"
-	sed -i.bak -E "s/package $pkg/package gw/g" ${gwfile}
-	sed -i.bak -E "s/protoReq /&$pkg\./g" ${gwfile}
-	sed -i.bak -E "s/, client /, client $pkg./g" ${gwfile}
-	sed -i.bak -E "s/Client /, client $pkg./g" ${gwfile}
-	sed -i.bak -E "s/[^(]*Client, runtime/${pkg}.&/" ${gwfile}
-	sed -i.bak -E "s/New[A-Za-z]*Client/${pkg}.&/" ${gwfile}
-	# darwin doesn't like newlines in sed...
-	sed -i.bak -E "s|import \(|& \"github.com/coreos/etcd/${pkgpath}\"|" ${gwfile}
-	mkdir -p  "${pkgpath}"/gw/
-	go fmt ${gwfile}
-	mv ${gwfile} "${pkgpath}/gw/"
-	rm -f ./etcdserver/${pb}*.bak
-	swaggerName=$(basename ${pb})
-	mv	Documentation/dev-guide/apispec/swagger/etcdserver/${pb}.swagger.json \
-		Documentation/dev-guide/apispec/swagger/"${swaggerName}".swagger.json
-done
-rm -rf Documentation/dev-guide/apispec/swagger/etcdserver/
-
-# append security to swagger spec
-go get -u "github.com/hexfusion/schwag"
-pushd "${SCHWAG_ROOT}"
-	git reset --hard "${SCHWAG_SHA}"
-	go install .
-popd
-schwag -input=Documentation/dev-guide/apispec/swagger/rpc.swagger.json
-
-# install protodoc
-# go get -v -u github.com/coreos/protodoc
-#
-# by default, do not run this option.
-# only run when './scripts/genproto.sh -g'
-#
-if [ "$1" = "-g" ]; then
-	echo "protodoc is auto-generating grpc API reference documentation..."
-	go get -v -u github.com/coreos/protodoc
-	SHA_PROTODOC="4372ee725035a208404e2d5465ba921469decc32"
-	PROTODOC_PATH="${GOPATH}/src/github.com/coreos/protodoc"
-	pushd "${PROTODOC_PATH}"
-		git reset --hard "${SHA_PROTODOC}"
-		go install
-		echo "protodoc is updated"
-	popd
-
-	protodoc --directories="etcdserver/etcdserverpb=service_message,mvcc/mvccpb=service_message,lease/leasepb=service_message,auth/authpb=service_message" \
-		--title="etcd API Reference" \
-		--output="Documentation/dev-guide/api_reference_v3.md" \
-		--message-only-from-this-file="etcdserver/etcdserverpb/rpc.proto" \
-		--disclaimer="This is a generated documentation. Please read the proto files for more."
-
-	protodoc --directories="etcdserver/api/v3lock/v3lockpb=service_message,etcdserver/api/v3election/v3electionpb=service_message,mvcc/mvccpb=service_message" \
-		--title="etcd concurrency API Reference" \
-		--output="Documentation/dev-guide/api_concurrency_reference_v3.md" \
-		--disclaimer="This is a generated documentation. Please read the proto files for more."
-
-	echo "protodoc is finished..."
-else
-	echo "skipping grpc API reference document auto-generation..."
-fi
