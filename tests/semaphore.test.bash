aff1ec7491e86c9f89cc9e5843053bf35f4afe0d
Gyuho Lee <leegyuho@amazon.com>
2019-08-15 12:50:08 -0700

    tests: debug
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- tests/semaphore.test.bash
+++ tests/semaphore.test.bash
@@ -14,4 +14,4 @@ sudo HOST_TMP_DIR=/tmp TEST_OPTS="PASSES='build release e2e' MANUAL_VER=v3.3.13"
 sudo HOST_TMP_DIR=/tmp TEST_OPTS="GOARCH=386 PASSES='build e2e'" make docker-test
 COMMENT
 
-sudo HOST_TMP_DIR=/tmp TEST_OPTS="PASSES='build release e2e' MANUAL_VER=v3.3.13" make docker-test
+sudo HOST_TMP_DIR=/tmp TEST_OPTS="EXPECT_DEBUG=1 PASSES='build release e2e' MANUAL_VER=v3.3.13" make docker-test
