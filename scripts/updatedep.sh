aa1e17aac3a6cad4dd1f62578ffe4c872b021693
Gyuho Lee <leegyuho@amazon.com>
2019-08-19 11:09:19 -0700

    go.mod: remove, change back to "glide"
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- scripts/updatedep.sh
+++ scripts/updatedep.sh
@@ -6,6 +6,17 @@ if ! [[ "$0" =~ scripts/updatedep.sh ]]; then
   exit 255
 fi
 
+# NOTE: just run
+rm -rf ./vendor
+glide install --strip-vendor --skip-test .
+glide vc --only-code --no-tests
+
+# ref. https://github.com/kubernetes/kubernetes/pull/81434
+exit 0
+
+
+
+
 if [ -d "gopath.proto" ]; then
   # gopath.proto is created by genproto.sh and it thoroughly messes
   # with go mod.
