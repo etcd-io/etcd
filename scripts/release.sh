c7e4e50879a597475a937f70e6f715e8b96407d2
Gyuho Lee <leegyuho@amazon.com>
2019-09-16 22:53:44 -0700

    scripts: remove "ACI" builds
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,11 +11,6 @@ if [ -z "${VERSION}" ]; then
 	exit 255
 fi
 
-if ! command -v acbuild >/dev/null; then
-    echo "cannot find acbuild"
-    exit 1
-fi
-
 if ! command -v docker >/dev/null; then
     echo "cannot find docker"
     exit 1
@@ -26,12 +21,6 @@ ETCD_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 pushd ${ETCD_ROOT} >/dev/null
 	echo Building etcd binary...
 	./scripts/build-binary ${VERSION}
-
-	# ppc64le not yet supported by acbuild.
-	for TARGET_ARCH in "amd64" "arm64"; do
-		echo Building ${TARGET_ARCH} aci image...
-		GOARCH=${TARGET_ARCH} BINARYDIR=release/etcd-${VERSION}-linux-${TARGET_ARCH} BUILDDIR=release ./scripts/build-aci ${VERSION}
-	done
 
 	for TARGET_ARCH in "amd64" "arm64" "ppc64le"; do
 		echo Building ${TARGET_ARCH} docker image...
