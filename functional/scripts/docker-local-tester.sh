fce0e23d1bd030de55f3e8a3ec282d5bc1d641ae
Gyuho Lee <leegyuho@amazon.com>
2019-09-27 23:37:11 -0700

    *: use Go 1.13.1
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- functional/scripts/docker-local-tester.sh
+++ functional/scripts/docker-local-tester.sh
@@ -6,7 +6,7 @@ if ! [[ "${0}" =~ "scripts/docker-local-tester.sh" ]]; then
 fi
 
 if [[ -z "${GO_VERSION}" ]]; then
-  GO_VERSION=1.13
+  GO_VERSION=1.13.1
 fi
 echo "Running with GO_VERSION:" ${GO_VERSION}
 
