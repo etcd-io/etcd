63116ffdb4945dad8306b8ded3797f4a6ba6b9da
shawwang <shawwang@tencent.com>
2020-02-24 15:47:45 +0800

    auth: add new metric 'etcd_debugging_auth_revision'

--- auth/store.go
+++ auth/store.go
@@ -1176,6 +1176,8 @@ func NewAuthStore(lg *zap.Logger, be backend.Backend, tp TokenProvider, bcryptCo
 		as.saveConsistentIndex(tx)
 	}
 
+	as.setupMetricsReporter()
+
 	tx.Unlock()
 	be.ForceCommit()
 
@@ -1461,4 +1463,12 @@ func (as *authStore) saveConsistentIndex(tx backend.BatchTx) {
 			plog.Error("failed to save consistentIndex,syncConsistentIndex is nil")
 		}
 	}
+}
+
+func (as *authStore) setupMetricsReporter() {
+	reportCurrentAuthRevMu.Lock()
+	reportCurrentAuthRev = func() float64 {
+		return float64(as.Revision())
+	}
+	reportCurrentAuthRevMu.Unlock()
 }
