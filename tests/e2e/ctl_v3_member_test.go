749123437e627a9a739ae1925be96c4057bc33c1
tangcong <tangcong506@gmail.com>
2020-04-25 22:03:48 +0800

    e2e: add memberListWithHexTest

--- tests/e2e/ctl_v3_member_test.go
+++ tests/e2e/ctl_v3_member_test.go
@@ -18,6 +18,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -25,6 +26,7 @@ import (
 )
 
 func TestCtlV3MemberList(t *testing.T)          { testCtl(t, memberListTest) }
+func TestCtlV3MemberListWithHex(t *testing.T)   { testCtl(t, memberListWithHexTest) }
 func TestCtlV3MemberListNoTLS(t *testing.T)     { testCtl(t, memberListTest, withCfg(configNoTLS)) }
 func TestCtlV3MemberListClientTLS(t *testing.T) { testCtl(t, memberListTest, withCfg(configClientTLS)) }
 func TestCtlV3MemberListClientAutoTLS(t *testing.T) {
@@ -108,6 +110,52 @@ func getMemberList(cx ctlCtx) (etcdserverpb.MemberListResponse, error) {
 		return etcdserverpb.MemberListResponse{}, err
 	}
 	return resp, nil
+}
+
+func memberListWithHexTest(cx ctlCtx) {
+	resp, err := getMemberList(cx)
+	if err != nil {
+		cx.t.Fatalf("getMemberList error (%v)", err)
+	}
+
+	cmdArgs := append(cx.PrefixArgs(), "--write-out", "json", "--hex", "member", "list")
+
+	proc, err := spawnCmd(cmdArgs)
+	if err != nil {
+		cx.t.Fatalf("memberListWithHexTest error (%v)", err)
+	}
+	var txt string
+	txt, err = proc.Expect("members")
+	if err != nil {
+		cx.t.Fatalf("memberListWithHexTest error (%v)", err)
+	}
+	if err = proc.Close(); err != nil {
+		cx.t.Fatalf("memberListWithHexTest error (%v)", err)
+	}
+	hexResp := etcdserverpb.MemberListResponse{}
+	dec := json.NewDecoder(strings.NewReader(txt))
+	if err := dec.Decode(&hexResp); err == io.EOF {
+		cx.t.Fatalf("memberListWithHexTest error (%v)", err)
+	}
+	num := len(resp.Members)
+	hexNum := len(hexResp.Members)
+	if num != hexNum {
+		cx.t.Fatalf("member number,expected %d,got %d", num, hexNum)
+	}
+	if num == 0 {
+		cx.t.Fatal("member number is 0")
+	}
+	for i := 0; i < num; i++ {
+		if resp.Members[i].Name != hexResp.Members[i].Name {
+			cx.t.Fatalf("member name,expected %v,got %v", resp.Members[i].Name, hexResp.Members[i].Name)
+		}
+		if !reflect.DeepEqual(resp.Members[i].PeerURLs, hexResp.Members[i].PeerURLs) {
+			cx.t.Fatalf("member peerURLs,expected %v,got %v", resp.Members[i].PeerURLs, hexResp.Members[i].PeerURLs)
+		}
+		if !reflect.DeepEqual(resp.Members[i].ClientURLs, hexResp.Members[i].ClientURLs) {
+			cx.t.Fatalf("member clientURLS,expected %v,got %v", resp.Members[i].ClientURLs, hexResp.Members[i].ClientURLs)
+		}
+	}
 }
 
 func memberRemoveTest(cx ctlCtx) {
