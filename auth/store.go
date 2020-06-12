490c6139ac64af0e7c7e9e9210e87e7de9b2fcb2
cfc4n <cfc4nphp@gmail.com>
2020-06-12 22:43:07 +0800

    auth: return incorrect result 'ErrUserNotFound' when client request without username or username was empty.
    Fiexs https://github.com/etcd-io/etcd/issues/12004 .

--- auth/store.go
+++ auth/store.go
@@ -995,7 +995,7 @@ func (as *authStore) IsAdminPermitted(authInfo *AuthInfo) error {
 	if !as.IsAuthEnabled() {
 		return nil
 	}
-	if authInfo == nil {
+	if authInfo == nil || authInfo.Username == "" {
 		return ErrUserEmpty
 	}
 
