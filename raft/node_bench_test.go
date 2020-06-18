ed39c95247ff764b5c457a3439dfa865fd85cfb5
Fullstop000 <fullstop1005@gmail.com>
2020-06-18 11:30:54 +0800

    re-add BenchmarkOneNode
    Signed-off-by: Fullstop000 <fullstop1005@gmail.com>

--- raft/node_bench_test.go
+++ raft/node_bench_test.go
@@ -22,12 +22,42 @@ import (
 	"time"
 )
 
+func BenchmarkOneNode(b *testing.B) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	s := NewMemoryStorage()
+	rn := newTestRawNode(1, []uint64{1}, 10, 1, s)
+	n := newNode(rn)
+	go n.run()
+
+	defer n.Stop()
+
+	n.Campaign(ctx)
+	go func() {
+		for i := 0; i < b.N; i++ {
+			n.Propose(ctx, []byte("foo"))
+		}
+	}()
+
+	for {
+		rd := <-n.Ready()
+		s.Append(rd.Entries)
+		// a reasonable disk sync latency
+		time.Sleep(1 * time.Millisecond)
+		n.Advance()
+		if rd.HardState.Commit == uint64(b.N+1) {
+			return
+		}
+	}
+}
+
 type tNode struct {
 	node *node
 	s    *MemoryStorage
 }
 
-func BenchmarkOneNode(b *testing.B) {
+func BenchmarkCluster(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
