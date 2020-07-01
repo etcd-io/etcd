87acbdd75a426503bdc3d23f5a67b8b08f40604f
tangcong <tangcong506@gmail.com>
2020-07-01 13:26:24 +0800

    etcdmain: fix shadow error

--- etcdmain/etcd.go
+++ etcdmain/etcd.go
@@ -270,7 +270,7 @@ func startProxy(cfg *config) error {
 
 	cfg.ec.Dir = filepath.Join(cfg.ec.Dir, "proxy")
 	if fileutil.Exist(cfg.ec.Dir) {
-		err := fileutil.CheckDirPermission(cfg.ec.Dir, fileutil.PrivateDirMode)
+		err = fileutil.CheckDirPermission(cfg.ec.Dir, fileutil.PrivateDirMode)
 		if err != nil {
 			return err
 		}
