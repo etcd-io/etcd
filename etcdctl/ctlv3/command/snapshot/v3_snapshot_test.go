a413b3b57ffecd6d1229999a93c42420094bc527
Piotr Tabor <ptab@google.com>
2020-08-25 01:22:12 +0200

    etcdctl: Move work on snapshot's file code to etcdctl.
    This logic is solelly used by etcdctl and depends on implementation
    details (files layout) of server so connot be kept in the client module.

--- etcdctl/ctlv3/command/snapshot/v3_snapshot_test.go
+++ etcdctl/ctlv3/command/snapshot/v3_snapshot_test.go
@@ -25,10 +25,10 @@ import (
 	"testing"
 	"time"
 
-	"go.etcd.io/etcd/v3/clientv3"
+	"go.etcd.io/etcd/c/v3/clientv3"
+	"go.etcd.io/etcd/c/v3/pkg/fileutil"
+	"go.etcd.io/etcd/c/v3/pkg/testutil"
 	"go.etcd.io/etcd/v3/embed"
-	"go.etcd.io/etcd/v3/pkg/fileutil"
-	"go.etcd.io/etcd/v3/pkg/testutil"
 
 	"go.uber.org/zap"
 )
