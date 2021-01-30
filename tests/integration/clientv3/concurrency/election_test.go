0d7a671c75fa378420090578c8dec95675a8b0ae
Piotr Tabor <ptab@google.com>
2021-01-31 00:00:26 +0100

    Tests: Fix vet warnings about Fatal in sub-goroutines.
    % (cd tests && go vet ./...)
    stderr: # go.etcd.io/etcd/tests/v3/integration/clientv3/concurrency_test
    stderr: integration/clientv3/concurrency/election_test.go:74:6: call to (*T).Fatal from a non-test goroutine
    stderr: integration/clientv3/concurrency/mutex_test.go:57:4: call to (*T).Fatal from a non-test goroutine

--- tests/integration/clientv3/concurrency/election_test.go
+++ tests/integration/clientv3/concurrency/election_test.go
@@ -65,13 +65,14 @@ func TestResumeElection(t *testing.T) {
 
 	respChan := make(chan *clientv3.GetResponse)
 	go func() {
+		defer close(respChan)
 		o := e.Observe(ctx)
 		respChan <- nil
 		for {
 			select {
 			case resp, ok := <-o:
 				if !ok {
-					t.Fatal("Observe() channel closed prematurely")
+					t.Error("Observe() channel closed prematurely")
 				}
 				// Ignore any observations that candidate1 was elected
 				if string(resp.Kvs[0].Value) == "candidate1" {
