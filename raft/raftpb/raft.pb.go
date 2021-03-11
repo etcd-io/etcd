8ff0ff836a05086532f7aee624dbace6eba9317a
Sam Batschelet <sbatsche@redhat.com>
2021-03-11 11:27:52 -0500

    *: regen proto
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

--- raft/raftpb/raft.pb.go
+++ raft/raftpb/raft.pb.go
@@ -67,6 +67,8 @@ func (EntryType) EnumDescriptor() ([]byte, []int) {
 	return fileDescriptor_b042552c306ae59b, []int{0}
 }
 
+// For description of different message types, see:
+// https://pkg.go.dev/go.etcd.io/etcd/raft/v3#hdr-MessageType
 type MessageType int32
 
 const (
@@ -388,21 +390,26 @@ func (m *Snapshot) XXX_DiscardUnknown() {
 var xxx_messageInfo_Snapshot proto.InternalMessageInfo
 
 type Message struct {
-	Type                 MessageType `protobuf:"varint,1,opt,name=type,enum=raftpb.MessageType" json:"type"`
-	To                   uint64      `protobuf:"varint,2,opt,name=to" json:"to"`
-	From                 uint64      `protobuf:"varint,3,opt,name=from" json:"from"`
-	Term                 uint64      `protobuf:"varint,4,opt,name=term" json:"term"`
-	LogTerm              uint64      `protobuf:"varint,5,opt,name=logTerm" json:"logTerm"`
-	Index                uint64      `protobuf:"varint,6,opt,name=index" json:"index"`
-	Entries              []Entry     `protobuf:"bytes,7,rep,name=entries" json:"entries"`
-	Commit               uint64      `protobuf:"varint,8,opt,name=commit" json:"commit"`
-	Snapshot             Snapshot    `protobuf:"bytes,9,opt,name=snapshot" json:"snapshot"`
-	Reject               bool        `protobuf:"varint,10,opt,name=reject" json:"reject"`
-	RejectHint           uint64      `protobuf:"varint,11,opt,name=rejectHint" json:"rejectHint"`
-	Context              []byte      `protobuf:"bytes,12,opt,name=context" json:"context,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}    `json:"-"`
-	XXX_unrecognized     []byte      `json:"-"`
-	XXX_sizecache        int32       `json:"-"`
+	Type MessageType `protobuf:"varint,1,opt,name=type,enum=raftpb.MessageType" json:"type"`
+	To   uint64      `protobuf:"varint,2,opt,name=to" json:"to"`
+	From uint64      `protobuf:"varint,3,opt,name=from" json:"from"`
+	Term uint64      `protobuf:"varint,4,opt,name=term" json:"term"`
+	// logTerm is generally used for appending Raft logs to followers. For example,
+	// (type=MsgApp,index=100,logTerm=5) means leader appends entries starting at
+	// index=101, and the term of entry at index 100 is 5.
+	// (type=MsgAppResp,reject=true,index=100,logTerm=5) means follower rejects some
+	// entries from its leader as it already has an entry with term 5 at index 100.
+	LogTerm              uint64   `protobuf:"varint,5,opt,name=logTerm" json:"logTerm"`
+	Index                uint64   `protobuf:"varint,6,opt,name=index" json:"index"`
+	Entries              []Entry  `protobuf:"bytes,7,rep,name=entries" json:"entries"`
+	Commit               uint64   `protobuf:"varint,8,opt,name=commit" json:"commit"`
+	Snapshot             Snapshot `protobuf:"bytes,9,opt,name=snapshot" json:"snapshot"`
+	Reject               bool     `protobuf:"varint,10,opt,name=reject" json:"reject"`
+	RejectHint           uint64   `protobuf:"varint,11,opt,name=rejectHint" json:"rejectHint"`
+	Context              []byte   `protobuf:"bytes,12,opt,name=context" json:"context,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
 }
 
 func (m *Message) Reset()         { *m = Message{} }
