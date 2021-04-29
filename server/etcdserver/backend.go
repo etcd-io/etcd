1145c576016fba4147c412e34ea5a28d78608993
wpedrak <wpedrak@google.com>
2021-04-29 12:08:52 +0200

    server: replace mlockall with `Mlock` in `--experimental-memory-mlock`
    Implementation of `--experimental-memory-mlock` backed by `mlockall` syscall is replaced by `Mlock` flag (backed by mlock syscall) of bboltDB.

--- server/etcdserver/backend.go
+++ server/etcdserver/backend.go
@@ -50,9 +50,6 @@ func newBackend(cfg config.ServerConfig) backend.Backend {
 		// permit 10% excess over quota for disarm
 		bcfg.MmapSize = uint64(cfg.QuotaBackendBytes + cfg.QuotaBackendBytes/10)
 	}
-	if cfg.ExperimentalMemoryMlock {
-		cfg.Logger.Info("setting `Mlock` bbolt flag")
-	}
 	bcfg.Mlock = cfg.ExperimentalMemoryMlock
 
 	return backend.New(bcfg)
