80407703f88ec31c25412c789140ae47b003a0c4
tyltr <tylitianrui@126.com>
2021-05-23 13:54:04 +0800

    comment

--- pkg/stringutil/convert.go
+++ pkg/stringutil/convert.go
@@ -2,6 +2,7 @@ package stringutil
 
 import "unsafe"
 
+// StringToBytes convert a string into  bytes without mem-allocs.
 func StringToBytes(s string) []byte {
 	return *(*[]byte)(unsafe.Pointer(&struct {
 		string
