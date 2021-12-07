661e0a91ef115fc5ebcb3bfc717161082f924525
ahrtr <nkty_coolnick@163.com>
2021-12-07 10:57:10 +0800

    added a new flag --rev to make-mirror command to support incremental mirror

--- etcdctl/ctlv3/command/make_mirror_command.go
+++ etcdctl/ctlv3/command/make_mirror_command.go
@@ -43,6 +43,7 @@ var (
 	mmuser         string
 	mmpassword     string
 	mmnodestprefix bool
+	mmrev          int64
 )
 
 // NewMakeMirrorCommand returns the cobra command for "makeMirror".
@@ -54,6 +55,7 @@ func NewMakeMirrorCommand() *cobra.Command {
 	}
 
 	c.Flags().StringVar(&mmprefix, "prefix", "", "Key-value prefix to mirror")
+	c.Flags().Int64Var(&mmrev, "rev", 0, "Specify the kv revision to start to mirror")
 	c.Flags().StringVar(&mmdestprefix, "dest-prefix", "", "destination prefix to mirror a prefix to a different prefix in the destination cluster")
 	c.Flags().BoolVar(&mmnodestprefix, "no-dest-prefix", false, "mirror key-values to the root of the destination cluster")
 	c.Flags().StringVar(&mmcert, "dest-cert", "", "Identify secure client using this TLS certificate file for the destination cluster")
@@ -142,28 +144,37 @@ func makeMirror(ctx context.Context, c *clientv3.Client, dc *clientv3.Client) er
 		}
 	}()
 
-	s := mirror.NewSyncer(c, mmprefix, 0)
-
-	rc, errc := s.SyncBase(ctx)
-
-	// if remove destination prefix is false and destination prefix is empty set the value of destination prefix same as prefix
-	if !mmnodestprefix && len(mmdestprefix) == 0 {
-		mmdestprefix = mmprefix
+	startRev := mmrev - 1
+	if startRev < 0 {
+		startRev = 0
 	}
 
-	for r := range rc {
-		for _, kv := range r.Kvs {
-			_, err := dc.Put(ctx, modifyPrefix(string(kv.Key)), string(kv.Value))
-			if err != nil {
-				return err
-			}
-			atomic.AddInt64(&total, 1)
+	s := mirror.NewSyncer(c, mmprefix, startRev)
+
+	// If a rev is provided, then do not sync the whole key space.
+	// Instead, just start watching the key space starting from the rev
+	if startRev == 0 {
+		rc, errc := s.SyncBase(ctx)
+
+		// if remove destination prefix is false and destination prefix is empty set the value of destination prefix same as prefix
+		if !mmnodestprefix && len(mmdestprefix) == 0 {
+			mmdestprefix = mmprefix
 		}
-	}
 
-	err := <-errc
-	if err != nil {
-		return err
+		for r := range rc {
+			for _, kv := range r.Kvs {
+				_, err := dc.Put(ctx, modifyPrefix(string(kv.Key)), string(kv.Value))
+				if err != nil {
+					return err
+				}
+				atomic.AddInt64(&total, 1)
+			}
+		}
+
+		err := <-errc
+		if err != nil {
+			return err
+		}
 	}
 
 	wc := s.SyncUpdates(ctx)
