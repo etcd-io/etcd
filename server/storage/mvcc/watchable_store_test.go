a45c73d9b12da436a4a36ac1c344492c1eb3ea67
Linhai <songlinhai0543@gmail.com>
2021-12-21 17:49:47 -0500

    resolve the conflict

--- server/storage/mvcc/watchable_store_test.go
+++ server/storage/mvcc/watchable_store_test.go
@@ -536,8 +536,8 @@ func TestWatchVictims(t *testing.T) {
 	s := newWatchableStore(zap.NewExample(), b, &lease.FakeLessor{}, StoreConfig{})
 
 	defer func() {
-		s.Close()
 		b.Close()
+		s.Close()
 		os.Remove(tmpPath)
 		chanBufLen, maxWatchersPerSync = oldChanBufLen, oldMaxWatchersPerSync
 	}()
