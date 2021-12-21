246e7eba095888a443dc115be1b032405c32086f
Linhai <songlinhai0543@gmail.com>
2021-12-21 04:46:39 -0500

    fixing the goroutine in two unit tests

--- server/storage/mvcc/watchable_store_test.go
+++ server/storage/mvcc/watchable_store_test.go
@@ -536,7 +536,8 @@ func TestWatchVictims(t *testing.T) {
 	s := newWatchableStore(zap.NewExample(), b, &lease.FakeLessor{}, StoreConfig{})
 
 	defer func() {
-		s.store.Close()
+		s.Close()
+		b.Close()
 		os.Remove(tmpPath)
 		chanBufLen, maxWatchersPerSync = oldChanBufLen, oldMaxWatchersPerSync
 	}()
