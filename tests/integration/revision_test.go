3788523a3e2dbfd3ca6552c97a77650516f4f7dc
Marek Siarkowicz <siarkowicz@google.com>
2022-01-24 15:33:30 +0100

    tests: Move revision tests out of concurrency

--- tests/integration/revision_test.go
+++ tests/integration/revision_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package concurrency_test
+package integration_test
 
 import (
 	"context"
