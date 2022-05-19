19941654fe03d1ccd2162379e2ebe2dba036e253
Marek Siarkowicz <siarkowicz@google.com>
2022-05-19 14:08:52 +0200

    server: Pass revision as int
    Signed-off-by: Marek Siarkowicz <siarkowicz@google.com>

--- server/storage/mvcc/kvstore_compaction.go
+++ server/storage/mvcc/kvstore_compaction.go
@@ -39,7 +39,7 @@ func (s *store) scheduleCompaction(compactMainRev, prevCompactRev int64) (uint32
 
 	batchNum := s.cfg.CompactionBatchLimit
 	batchInterval := s.cfg.CompactionSleepInterval
-	h := newKVHasher(revision{main: prevCompactRev + 1}, revision{main: compactMainRev + 1}, keep)
+	h := newKVHasher(prevCompactRev+1, compactMainRev+1, keep)
 	last := make([]byte, 8+1+8)
 	for {
 		var rev revision
