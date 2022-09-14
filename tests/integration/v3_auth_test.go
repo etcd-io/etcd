e8ad008067b7255ff1f5fffae1c1c0a7ceda933e
Benjamin Wang <wachao@vmware.com>
2022-09-14 14:30:37 +0800

    integration_test: fix the flaky test TestV3AuthRestartMembe
    Signed-off-by: Benjamin Wang <wachao@vmware.com>

--- tests/integration/v3_auth_test.go
+++ tests/integration/v3_auth_test.go
@@ -492,6 +492,7 @@ func TestV3AuthRestartMember(t *testing.T) {
 	clus.Members[0].Stop(t)
 	err = clus.Members[0].Restart(t)
 	testutil.AssertNil(t, err)
+	integration.WaitClientV3WithKey(t, c2.KV, "foo")
 
 	// nothing has changed, but it fails without refreshing cache after restart
 	_, err = c2.Put(context.TODO(), "foo", "bar2")
