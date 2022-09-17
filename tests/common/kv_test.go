d657f2f66bcd5739654bd58d9ad24c8a00568ffc
Benjamin Wang <wachao@vmware.com>
2022-09-18 04:26:30 +0800

    Test: increase the TestKVDelete's timeout to 15s
    Sometimes it just needs more time to execute the test case,
    so increase the timeout from 10s to 15s.
    
    Signed-off-by: Benjamin Wang <wachao@vmware.com>

--- tests/common/kv_test.go
+++ tests/common/kv_test.go
@@ -123,7 +123,7 @@ func TestKVDelete(t *testing.T) {
 	testRunner.BeforeTest(t)
 	for _, tc := range clusterTestCases {
 		t.Run(tc.name, func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 			defer cancel()
 			clus := testRunner.NewCluster(ctx, t, tc.config)
 			defer clus.Close()
