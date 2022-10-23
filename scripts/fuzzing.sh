bbda80486f7eef742b9f9500094b6524c6ad42a6
Samuele Resca <samuele.resca@gmail.com>
2022-10-23 13:45:22 +0100

    Updating script for static analysis readiness.
    Signed-off-by: Samuele Resca <samuele.resca@gmail.com>

--- scripts/fuzzing.sh
+++ scripts/fuzzing.sh
@@ -10,8 +10,8 @@ TARGETS="FuzzTxnRangeRequest  FuzzTxnPutRequest  FuzzTxnDeleteRangeRequest"
 
 for target in ${TARGETS}; do
     log_callout -e "\\nExecuting fuzzing with target ${target} in $target_path with a timeout of $fuzz_time\\n"
-    run pushd ${target_path}
-        $GO_CMD test -fuzz ${target} -fuzztime ${fuzz_time}
+    run pushd "${target_path}"
+        $GO_CMD test -fuzz "${target}" -fuzztime "${fuzz_time}"
     run popd
     log_success -e "\\COMPLETED: fuzzing with target $target in $target_path \\n"
 done
