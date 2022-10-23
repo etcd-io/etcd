37d62ba4770b45857b90822df31def0a673d28e5
Samuele Resca <sr7@ad.datcon.co.uk>
2022-10-23 12:48:21 +0100

    Updating fuzzing script with list of target.
    Signed-off-by: Samuele Resca <samuele.resca@gmail.com>

--- scripts/fuzzing.sh
+++ scripts/fuzzing.sh
@@ -5,9 +5,14 @@ source ./scripts/test_lib.sh
 GO_CMD="go"
 fuzz_time=${FUZZ_TIME:-"300s"}
 target_path=${TARGET_PATH:-"./server/etcdserver/api/v3rpc"}
-fuzz_target=${FUZZ_TARGET:-"FuzzRangeRequest"}
+TARGETS="FuzzTxnRangeRequest  FuzzTxnPutRequest  FuzzTxnDeleteRangeRequest"
 
-log_callout -e "\\nExecuting fuzzing with target ${fuzz_target} in $target_path with a timeout of $fuzz_time\\n"
-cd "$target_path"
-$GO_CMD test -fuzz "$fuzz_target" -fuzztime "$fuzz_time"
-log_success -e "\\COMPLETED: fuzzing with target $fuzz_target in $target_path \\n"
+
+for target in ${TARGETS}; do
+    log_callout -e "\\nExecuting fuzzing with target ${target} in $target_path with a timeout of $fuzz_time\\n"
+    run pushd ${target_path}
+        $GO_CMD test -fuzz ${target} -fuzztime ${fuzz_time}
+    run popd
+    log_success -e "\\COMPLETED: fuzzing with target $target in $target_path \\n"
+done
+
