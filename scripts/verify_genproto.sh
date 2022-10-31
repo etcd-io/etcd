a774510a90f5ff96fbc0f43335db5c76385771a0
spacewander <spacewanderlzx@gmail.com>
2022-10-31 19:49:54 +0800

    address review
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- scripts/verify_genproto.sh
+++ scripts/verify_genproto.sh
@@ -6,14 +6,22 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+tmpWorkDir=$(mktemp -d -t 'twd.XXXXXX')
+mkdir "$tmpWorkDir/etcd"
+tmpWorkDir="$tmpWorkDir/etcd"
+cp -r . "$tmpWorkDir"
+pushd "$tmpWorkDir"
+git add -A
+git commit -m init || true # maybe fail because nothing to commit 
 ./scripts/genproto.sh
-diff=$(git diff --name-only | grep -c ".pb.")
-if [ "$diff" -eq 0 ]; then
+diff=$(git diff --numstat | awk '{print $3}')
+popd
+if [ -z "$diff" ]; then
   echo "PASSED genproto-verification!"
   exit 0
 fi
 echo "Failed genproto-verification!" >&2
-echo "* Found changed files $(git diff --name-only | grep '.pb.')" >&2
+printf "* Found changed files:\n%s\n" "$diff" >&2
 echo "* Please rerun genproto.sh after changing *.proto file" >&2
 echo "* Run ./scripts/genproto.sh" >&2
 exit 1
