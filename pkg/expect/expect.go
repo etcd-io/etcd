23a4d661d8693cf6c9c05871a9d01260d5c6a304
anushkamittal20 <anumittal4641@gmail.com>
2023-01-27 18:22:39 +0530

    expect: enable writing linearizability test etcd logs to files
    Signed-off-by: anushkamittal20 <anumittal4641@gmail.com>

--- pkg/expect/expect.go
+++ pkg/expect/expect.go
@@ -151,29 +151,18 @@ func (ep *ExpectProcess) tryReadNextLine(r *bufio.Reader) error {
 	}
 	defer out.Close()
 	files := []string{"logs.txt"}
-	// Create the archive and write the output to the "out" Writer
 	err4 := createArchive(files, out)
 	if err != nil {
 		fmt.Printf("error creating archive: %v", err4)
-
 	}
-
-	fmt.Println("Archive created successfully")
-	// we're checking the error here at the bottom to ensure any leftover reads are still taken into account
 	return err
 }
 
 func createArchive(files []string, buf io.Writer) error {
-	// Create new Writers for gzip and tar
-	// These writers are chained. Writing to the tar writer will
-	// write to the gzip writer which in turn will write to
-	// the "buf" writer
 	gw := gzip.NewWriter(buf)
 	defer gw.Close()
 	tw := tar.NewWriter(gw)
 	defer tw.Close()
-
-	// Iterate over files and add them to the tar archive
 	for _, file := range files {
 		err := addToArchive(tw, file)
 		if err != nil {
@@ -185,38 +174,24 @@ func createArchive(files []string, buf io.Writer) error {
 }
 
 func addToArchive(tw *tar.Writer, filename string) error {
-	// Open the file which will be written into the archive
 	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-
-	// Get FileInfo about our file providing file size, mode, etc.
 	info, err := file.Stat()
 	if err != nil {
 		return err
 	}
-
-	// Create a tar Header from the FileInfo data
 	header, err := tar.FileInfoHeader(info, info.Name())
 	if err != nil {
 		return err
 	}
-
-	// Use full path as name (FileInfoHeader only takes the basename)
-	// If we don't do this the directory strucuture would
-	// not be preserved
-	// https://golang.org/src/archive/tar/common.go?#L626
 	header.Name = filename
-
-	// Write file header to the tar archive
 	err = tw.WriteHeader(header)
 	if err != nil {
 		return err
 	}
-
-	// Copy file content to tar archive
 	_, err = io.Copy(tw, file)
 	if err != nil {
 		return err
