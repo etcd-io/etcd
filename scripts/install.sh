5464d34d42e87e4ea2e1cca53097694b513f3b0c
Prasad Chandrasekaran <prasadc@vmware.com>
2023-02-09 13:59:45 +0530

    Fix shellchecks and comment unsued url.
    Signed-off-by: Prasad Chandrasekaran <prasadc@vmware.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,22 +15,22 @@ install_direct () {
 	echo "Installing for ${machine}"
     # choose either URL
     GOOGLE_URL=https://storage.googleapis.com/etcd
-    GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
+    # GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
     DOWNLOAD_URL=${GOOGLE_URL}
 
 	# cleanup
-    rm -f /tmp/etcd-${ETCD_VER}-${machine}-amd64.tar.gz
+    rm -f "/tmp/etcd-${ETCD_VER}-${machine}-amd64.tar.gz"
     rm -rf /tmp/etcd-download-test && mkdir -p /tmp/etcd-download-test
 
 	# install 
     if [ "$machine" == "linux" ]; then
-        curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-${machine}-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-${machine}-amd64.tar.gz
-        tar xzvf /tmp/etcd-${ETCD_VER}-${machine}-amd64.tar.gz -C /tmp/etcd-download-test --strip-components=1
-        rm -f /tmp/etcd-${ETCD_VER}-${machine}-amd64.tar.gz
+        curl -L "${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-${machine}-amd64.tar.gz" -o "/tmp/etcd-${ETCD_VER}-${machine}-amd64.tar.gz"
+        tar xzvf "/tmp/etcd-${ETCD_VER}-${machine}-amd64.tar.gz" -C "/tmp/etcd-download-test --strip-components=1"
+        rm -f "/tmp/etcd-${ETCD_VER}-${machine}-amd64.tar.gz"
     else
-		curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-${machine}-amd64.zip -o /tmp/etcd-${ETCD_VER}-${machine}-amd64.zip
-		unzip /tmp/etcd-${ETCD_VER}-${machine}-amd64.zip -d /tmp && rm -f /tmp/etcd-${ETCD_VER}-${machine}-amd64.zip
-		mv /tmp/etcd-${ETCD_VER}-${machine}-amd64/* /tmp/etcd-download-test && rm -rf mv /tmp/etcd-${ETCD_VER}-${machine}-amd64
+		curl -L "${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-${machine}-amd64.zip" -o "/tmp/etcd-${ETCD_VER}-${machine}-amd64.zip"
+		unzip "/tmp/etcd-${ETCD_VER}-${machine}-amd64.zip" -d /tmp && rm -f "/tmp/etcd-${ETCD_VER}-${machine}-amd64.zip"
+		mv /tmp/etcd-"${ETCD_VER}"-"${machine}"-amd64/* /tmp/etcd-download-test && rm -rf mv "/tmp/etcd-${ETCD_VER}-${machine}-amd64"
     fi
 
 	# test
@@ -48,13 +48,13 @@ install_direct () {
 
 install_docker() {
 	rm -rf /tmp/etcd-data.tmp && mkdir -p /tmp/etcd-data.tmp && \
-		docker rmi gcr.io/etcd-development/etcd:${ETCD_VER} || true && \
+		docker rmi gcr.io/etcd-development/etcd:"${ETCD_VER}" || true && \
 		docker run \
 		-p 2379:2379 \
 		-p 2380:2380 \
 		--mount type=bind,source=/tmp/etcd-data.tmp,destination=/etcd-data \
-		--name etcd-gcr-${ETCD_VER} \
-		gcr.io/etcd-development/etcd:${ETCD_VER} \
+		--name etcd-gcr-"${ETCD_VER}" \
+		gcr.io/etcd-development/etcd:"${ETCD_VER}" \
 		/usr/local/bin/etcd \
 		--name s1 \
 		--data-dir /etcd-data \
@@ -70,12 +70,12 @@ install_docker() {
 		--log-outputs stderr
 
     # Remove the following ? we never reach this part as docker run spawns an etcd daemon
-	docker exec etcd-gcr-${ETCD_VER} /bin/sh -c "/usr/local/bin/etcd --version"
-	docker exec etcd-gcr-${ETCD_VER} /bin/sh -c "/usr/local/bin/etcdctl version"
-	docker exec etcd-gcr-${ETCD_VER} /bin/sh -c "/usr/local/bin/etcdutl version"
-	docker exec etcd-gcr-${ETCD_VER} /bin/sh -c "/usr/local/bin/etcdctl endpoint health"
-	docker exec etcd-gcr-${ETCD_VER} /bin/sh -c "/usr/local/bin/etcdctl put foo bar"
-	docker exec etcd-gcr-${ETCD_VER} /bin/sh -c "/usr/local/bin/etcdctl get foo"
+	docker exec etcd-gcr-"${ETCD_VER}" /bin/sh -c "/usr/local/bin/etcd --version"
+	docker exec etcd-gcr-"${ETCD_VER}" /bin/sh -c "/usr/local/bin/etcdctl version"
+	docker exec etcd-gcr-"${ETCD_VER}" /bin/sh -c "/usr/local/bin/etcdutl version"
+	docker exec etcd-gcr-"${ETCD_VER}" /bin/sh -c "/usr/local/bin/etcdctl endpoint health"
+	docker exec etcd-gcr-"${ETCD_VER}" /bin/sh -c "/usr/local/bin/etcdctl put foo bar"
+	docker exec etcd-gcr-"${ETCD_VER}" /bin/sh -c "/usr/local/bin/etcdctl get foo"
 }
 
 if [ "$#" -lt 1 ]; then
