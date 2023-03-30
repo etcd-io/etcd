3c40a68d09ff9ddab1db57e22babe4f8df5bf7ee
James Blair <mail@jamesblair.net>
2023-03-30 15:37:09 +1300

    Remove nocov flags for e2e tests.
    Signed-off-by: James Blair <mail@jamesblair.net>

--- tests/e2e/v3_cipher_suite_test.go
+++ tests/e2e/v3_cipher_suite_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !cov && !cluster_proxy
+//go:build !cluster_proxy
 
 package e2e
 
