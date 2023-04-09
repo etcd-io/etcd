85b4da6121356b9b5b079546b97d18a108594a52
Wei Fu <fuweid89@gmail.com>
2023-04-09 12:57:32 +0800

    chore: remove pkg/expect TODO comment
    https://github.com/google/goexpect has been archived on Feb 7, 2023.
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- pkg/expect/expect.go
+++ pkg/expect/expect.go
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 // Package expect implements a small expect-style interface
-// TODO(ptab): Consider migration to https://github.com/google/goexpect.
 package expect
 
 import (
