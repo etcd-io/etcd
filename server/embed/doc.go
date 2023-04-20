55fd983c44f406032144ca9e1d72fe303b3eb6a3
Juhee Kang <claudiajkang@gmail.com>
2023-04-20 14:13:21 +0200

    document: add stop logic of embedded etcd
    Add stop logic before closing in embedded etcd.
    It's followup of #15724.
    
    Signed-off-by: Juhee Kang <claudiajkang@gmail.com>

--- server/embed/doc.go
+++ server/embed/doc.go
@@ -31,13 +31,15 @@ Launch an embedded etcd server using the configuration defaults:
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		if e.Server != nil {
+			e.Server.Stop()
+		}
+
 		defer e.Close()
 		select {
 		case <-e.Server.ReadyNotify():
 			log.Printf("Server is ready!")
-		case <-time.After(60 * time.Second):
-			e.Server.Stop() // trigger a shutdown
-			log.Printf("Server took too long to start!")
 		}
 		log.Fatal(<-e.Err())
 	}
