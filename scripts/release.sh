aa373e54df6923b27cc0d2cde9c81cfa0045b377
Marek Siarkowicz <siarkowicz@google.com>
2023-05-09 12:18:29 +0200

    Move go version to dedicated .go-version file
    Signed-off-by: Marek Siarkowicz <siarkowicz@google.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -112,10 +112,10 @@ main() {
   # Check go version.
   log_callout "Check go version"
   local go_version current_go_version
-  go_version="go$(grep -oP '(?<=GO_VERSION:\s")[^"]*' .github/workflows/go-version.yaml | tr -d ' ')"
+  go_version="go$(cat .go-version)"
   current_go_version=$(go version | awk '{ print $3 }')
   if [[ "${current_go_version}" != "${go_version}" ]]; then
-    log_error "Current go version is ${current_go_version}, but etcd ${RELEASE_VERSION} requires ${go_version} (see .github/workflows/build.yaml)."
+    log_error "Current go version is ${current_go_version}, but etcd ${RELEASE_VERSION} requires ${go_version} (see .go-version)."
     exit 1
   fi
 
