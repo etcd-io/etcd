53af854871abb0e21357a091211ef3b002be56bc
Marek Siarkowicz <serathius@users.noreply.github.com>
2023-06-09 22:29:27 +0200

    tests/robustness: Assume starting from empty etcd instead of throwing out first failed request
    Signed-off-by: Marek Siarkowicz <serathius@users.noreply.github.com>

--- tests/robustness/model/non_deterministic_test.go
+++ tests/robustness/model/non_deterministic_test.go
@@ -19,6 +19,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"go.etcd.io/etcd/api/v3/mvccpb"
 )
 
 func TestModelNonDeterministic(t *testing.T) {
@@ -28,6 +30,22 @@ func TestModelNonDeterministic(t *testing.T) {
 	}
 
 	nonDeterministicTestScenarios = append(nonDeterministicTestScenarios, []nonDeterministicModelTest{
+		{
+			name: "First Put request fails, but is persisted",
+			operations: []nonDeterministicOperation{
+				{req: putRequest("key1", "1"), resp: failedResponse(errors.New("failed"))},
+				{req: putRequest("key2", "2"), resp: putResponse(3)},
+				{req: rangeRequest("key", true, 0), resp: rangeResponse([]*mvccpb.KeyValue{{Key: []byte("key1"), Value: []byte("1"), ModRevision: 2}, {Key: []byte("key2"), Value: []byte("2"), ModRevision: 3}}, 2, 3)},
+			},
+		},
+		{
+			name: "First Put request fails, and is lost",
+			operations: []nonDeterministicOperation{
+				{req: putRequest("key1", "1"), resp: failedResponse(errors.New("failed"))},
+				{req: putRequest("key2", "2"), resp: putResponse(2)},
+				{req: rangeRequest("key", true, 0), resp: rangeResponse([]*mvccpb.KeyValue{{Key: []byte("key2"), Value: []byte("2"), ModRevision: 2}}, 1, 2)},
+			},
+		},
 		{
 			name: "Put can fail and be lost before get",
 			operations: []nonDeterministicOperation{
