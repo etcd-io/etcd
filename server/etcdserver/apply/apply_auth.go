84a9af17cca4a1ace967f308c2b70692bfe19db3
Thomas Jungblut <tjungblu@redhat.com>
2023-06-15 15:57:44 +0200

    Add first unit test for authApplierV3
    This contains a slight refactoring to expose enough information
    to write meaningful tests for auth applier v3.
    
    Signed-off-by: Thomas Jungblut <tjungblu@redhat.com>

--- server/etcdserver/apply/apply_auth.go
+++ server/etcdserver/apply/apply_auth.go
@@ -125,19 +125,24 @@ func (aa *authApplierV3) LeaseRevoke(lc *pb.LeaseRevokeRequest) (*pb.LeaseRevoke
 func (aa *authApplierV3) checkLeasePuts(leaseID lease.LeaseID) error {
 	l := aa.lessor.Lookup(leaseID)
 	if l != nil {
-		// early return for most-common scenario of either disabled auth or admin user.
-		// IsAdminPermitted also checks whether auth is enabled
-		if err := aa.as.IsAdminPermitted(&aa.authInfo); err == nil {
-			return nil
-		}
-
-		for _, key := range l.Keys() {
-			if err := aa.as.IsPutPermitted(&aa.authInfo, []byte(key)); err != nil {
-				return err
-			}
-		}
+		return aa.checkLeasePutsKeys(l)
 	}
 
+	return nil
+}
+
+func (aa *authApplierV3) checkLeasePutsKeys(l *lease.Lease) error {
+	// early return for most-common scenario of either disabled auth or admin user.
+	// IsAdminPermitted also checks whether auth is enabled
+	if err := aa.as.IsAdminPermitted(&aa.authInfo); err == nil {
+		return nil
+	}
+
+	for _, key := range l.Keys() {
+		if err := aa.as.IsPutPermitted(&aa.authInfo, []byte(key)); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
