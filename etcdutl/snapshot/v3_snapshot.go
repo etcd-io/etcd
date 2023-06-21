2921992cbd1a5b62e713d0a5bba67c94cfaa87ab
shitao.lst <acm_tach@163.com>
2023-06-21 17:50:37 +0800

    [3.5] etcdutl: fix db double closed
    Signed-off-by: shitao.lst <acm_tach@163.com>

--- etcdutl/snapshot/v3_snapshot.go
+++ etcdutl/snapshot/v3_snapshot.go
@@ -415,13 +415,8 @@ func (s *v3Manager) copyAndVerifyDB() error {
 	if dberr != nil {
 		return dberr
 	}
-	dbClosed := false
-	defer func() {
-		if !dbClosed {
-			db.Close()
-			dbClosed = true
-		}
-	}()
+	defer db.Close()
+
 	if _, err := io.Copy(db, srcf); err != nil {
 		return err
 	}
@@ -458,7 +453,7 @@ func (s *v3Manager) copyAndVerifyDB() error {
 	}
 
 	// db hash is OK, can now modify DB so it can be part of a new cluster
-	db.Close()
+
 	return nil
 }
 
