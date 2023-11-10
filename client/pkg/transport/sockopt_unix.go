c48738e33fe1060ce8aaf775cdb24f36451538a8
Icemap <qizhi.wang@pingcap.com>
2023-11-11 00:05:36 +0800

    fix: add js platform
        Signed-off-by: Icemap <qizhi.wang@pingcap.com>
    
    Signed-off-by: Icemap <qizhi.wang@pingcap.com>

--- client/pkg/transport/sockopt_unix.go
+++ client/pkg/transport/sockopt_unix.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !windows && !solaris && !wasm
+//go:build !windows && !solaris && !wasm && !js
 
 package transport
 
