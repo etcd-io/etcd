c9d515ac79716c8e90f0bd403b354a332577ff51
Marek Siarkowicz <siarkowicz@google.com>
2024-01-15 17:19:05 +0100

    Try reproduce https://github.com/etcd-io/etcd/issues/17247
    Change-Id: Ieed5f16ded627ac27f6acae99fea23211e6f718d
    Signed-off-by: Marek Siarkowicz <siarkowicz@google.com>

--- tests/robustness/scenarios.go
+++ tests/robustness/scenarios.go
@@ -33,18 +33,6 @@ type TrafficProfile struct {
 
 var trafficProfiles = []TrafficProfile{
 	{
-		Traffic: traffic.EtcdPut,
-		Profile: traffic.HighTrafficProfile,
-	},
-	{
-		Traffic: traffic.EtcdPutDeleteLease,
-		Profile: traffic.LowTraffic,
-	},
-	{
-		Traffic: traffic.Kubernetes,
-		Profile: traffic.HighTrafficProfile,
-	},
-	{
 		Traffic: traffic.Kubernetes,
 		Profile: traffic.LowTraffic,
 	},
@@ -64,7 +52,6 @@ func scenarios(t *testing.T) []testScenario {
 	if err != nil {
 		t.Fatalf("Failed checking etcd version binary, binary: %q, err: %v", e2e.BinPath.Etcd, err)
 	}
-	enableLazyFS := e2e.BinPath.LazyFSAvailable()
 	randomizableOptions := []e2e.EPClusterOption{
 		options.WithClusterOptionGroups(
 			options.ClusterOptions{options.WithTickMs(29), options.WithElectionMs(271)},
@@ -73,29 +60,13 @@ func scenarios(t *testing.T) []testScenario {
 	}
 
 	baseOptions := []e2e.EPClusterOption{
-		options.WithSnapshotCount(50, 100, 1000),
+		options.WithSnapshotCount(50),
 		options.WithSubsetOptions(randomizableOptions...),
 		e2e.WithGoFailEnabled(true),
 		e2e.WithCompactionBatchLimit(100),
 		e2e.WithWatchProcessNotifyInterval(100 * time.Millisecond),
 	}
 	scenarios := []testScenario{}
-	for _, tp := range trafficProfiles {
-		name := filepath.Join(tp.Traffic.Name(), tp.Profile.Name, "ClusterOfSize1")
-		clusterOfSize1Options := baseOptions
-		clusterOfSize1Options = append(clusterOfSize1Options, e2e.WithClusterSize(1))
-		// Add LazyFS only for traffic with lower QPS as it uses a lot of CPU lowering minimal QPS.
-		if enableLazyFS && tp.Profile.MinimalQPS <= 100 {
-			clusterOfSize1Options = append(clusterOfSize1Options, e2e.WithLazyFSEnabled(true))
-			name = filepath.Join(name, "LazyFS")
-		}
-		scenarios = append(scenarios, testScenario{
-			name:    name,
-			traffic: tp.Traffic,
-			profile: tp.Profile,
-			cluster: *e2e.NewConfig(clusterOfSize1Options...),
-		})
-	}
 
 	for _, tp := range trafficProfiles {
 		name := filepath.Join(tp.Traffic.Name(), tp.Profile.Name, "ClusterOfSize3")
@@ -110,65 +81,6 @@ func scenarios(t *testing.T) []testScenario {
 			traffic: tp.Traffic,
 			profile: tp.Profile,
 			cluster: *e2e.NewConfig(clusterOfSize3Options...),
-		})
-	}
-	scenarios = append(scenarios, testScenario{
-		name:      "Issue14370",
-		failpoint: failpoint.RaftBeforeSavePanic,
-		profile:   traffic.LowTraffic,
-		traffic:   traffic.EtcdPutDeleteLease,
-		cluster: *e2e.NewConfig(
-			options.WithSubsetOptions(randomizableOptions...),
-			e2e.WithClusterSize(1),
-			e2e.WithGoFailEnabled(true),
-		),
-	})
-	scenarios = append(scenarios, testScenario{
-		name:      "Issue14685",
-		failpoint: failpoint.DefragBeforeCopyPanic,
-		profile:   traffic.LowTraffic,
-		traffic:   traffic.EtcdPutDeleteLease,
-		cluster: *e2e.NewConfig(
-			options.WithSubsetOptions(randomizableOptions...),
-			e2e.WithClusterSize(1),
-			e2e.WithGoFailEnabled(true),
-		),
-	})
-	scenarios = append(scenarios, testScenario{
-		name:      "Issue13766",
-		failpoint: failpoint.KillFailpoint,
-		profile:   traffic.HighTrafficProfile,
-		traffic:   traffic.EtcdPut,
-		cluster: *e2e.NewConfig(
-			e2e.WithSnapshotCount(100),
-		),
-	})
-	scenarios = append(scenarios, testScenario{
-		name: "Issue15220",
-		watch: watchConfig{
-			requestProgress: true,
-		},
-		profile: traffic.LowTraffic,
-		traffic: traffic.EtcdPutDeleteLease,
-		cluster: *e2e.NewConfig(
-			options.WithSubsetOptions(randomizableOptions...),
-			e2e.WithClusterSize(1),
-		),
-	})
-	// TODO: Deflake waiting for waiting until snapshot for etcd versions that don't support setting snapshot catchup entries.
-	if v.Compare(version.V3_6) >= 0 {
-		scenarios = append(scenarios, testScenario{
-			name:      "Issue15271",
-			failpoint: failpoint.BlackholeUntilSnapshot,
-			profile:   traffic.HighTrafficProfile,
-			traffic:   traffic.EtcdPut,
-			cluster: *e2e.NewConfig(
-				options.WithSubsetOptions(randomizableOptions...),
-				e2e.WithSnapshotCatchUpEntries(100),
-				e2e.WithSnapshotCount(100),
-				e2e.WithPeerProxy(true),
-				e2e.WithIsPeerTLS(true),
-			),
 		})
 	}
 	return scenarios
