da7ab15f80c06373e03267e11497da71a028a3aa
Ivan Valdes <ivan@vald.es>
2024-03-09 15:07:25 -0800

    tests: temporary disable coverage redisign Go experiment
    Go v1.22 has an error generating the coverage output. Disable it
    temporarily so GitHub workflows can run in the meantime.
    
    Signed-off-by: Ivan Valdes <ivan@vald.es>

--- scripts/codecov_upload.sh
+++ scripts/codecov_upload.sh
@@ -8,7 +8,7 @@ set -o pipefail
 LOG_FILE=${1:-test-coverage.log}
 
 # We collect the coverage
-COVERDIR=covdir PASSES='build cov' ./scripts/test.sh 2>&1 | tee "${LOG_FILE}"
+GOEXPERIMENT=nocoverageredesign COVERDIR=covdir PASSES='build cov' ./scripts/test.sh 2>&1 | tee "${LOG_FILE}"
 test_success="$?"
 
 # We try to upload whatever we have:
