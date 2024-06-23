eeeda9e6892836ca98955fe0cd2d75a7c093c5c2
Marek Siarkowicz <siarkowicz@google.com>
2024-06-23 23:08:07 +0200

    Unify calculating returnTime
    Signed-off-by: Marek Siarkowicz <siarkowicz@google.com>

--- tests/robustness/validate/patch_history.go
+++ tests/robustness/validate/patch_history.go
@@ -26,9 +26,9 @@ import (
 func patchLinearizableOperations(reports []report.ClientReport, persistedRequests []model.EtcdRequest) []porcupine.Operation {
 	allOperations := relevantOperations(reports)
 	watchRevision := requestRevision(reports)
-	watchReturnTime := watchReturnTime(reports)
-	persistedReturnTime := persistedReturnTime(allOperations, persistedRequests)
-	return patchOperations(allOperations, watchRevision, watchReturnTime, persistedReturnTime)
+	returnTime := returnTime(allOperations, reports, persistedRequests)
+	persistedRequestsCount := countPersistedRequests(persistedRequests)
+	return patchOperations(allOperations, watchRevision, returnTime, persistedRequestsCount)
 }
 
 func relevantOperations(reports []report.ClientReport) []porcupine.Operation {
@@ -46,7 +46,7 @@ func relevantOperations(reports []report.ClientReport) []porcupine.Operation {
 	return ops
 }
 
-func patchOperations(operations []porcupine.Operation, watchRevision, watchReturnTime, persistedReturnTime map[keyValue]int64) []porcupine.Operation {
+func patchOperations(operations []porcupine.Operation, watchRevision, returnTime, persistedRequestCount map[keyValue]int64) []porcupine.Operation {
 	newOperations := make([]porcupine.Operation, 0, len(operations))
 
 	for _, op := range operations {
@@ -67,15 +67,11 @@ func patchOperations(operations []porcupine.Operation, watchRevision, watchRetur
 				if ok {
 					txnRevision = revision
 				}
-				if t, ok := watchReturnTime[kv]; ok && t < op.Return {
+				if t, ok := returnTime[kv]; ok && t < op.Return {
 					op.Return = t
 				}
-				persistedReturnTime, ok := persistedReturnTime[kv]
+				_, ok = persistedRequestCount[kv]
 				if ok {
-					// Set return time based on persisted return time.
-					if persistedReturnTime < op.Return {
-						op.Return = persistedReturnTime
-					}
 					txnPersisted = true
 				}
 			case model.DeleteOperation:
@@ -124,48 +120,10 @@ func hasUniqueWriteOperation(ops []model.EtcdOperation) bool {
 	return false
 }
 
-func persistedReturnTime(allOperations []porcupine.Operation, persistedRequests []model.EtcdRequest) map[keyValue]int64 {
-	operationReturnTime := operationReturnTime(allOperations)
-	persisted := map[keyValue]int64{}
-
-	lastReturnTime := maxReturnTime(operationReturnTime)
-
-	for i := len(persistedRequests) - 1; i >= 0; i-- {
-		request := persistedRequests[i]
-		switch request.Type {
-		case model.Txn:
-			hasPut := false
-			lastReturnTime--
-			for _, op := range request.Txn.OperationsOnSuccess {
-				if op.Type != model.PutOperation {
-					continue
-				}
-				kv := keyValue{Key: op.Put.Key, Value: op.Put.Value}
-				if _, found := persisted[kv]; found {
-					panic(fmt.Sprintf("Unexpected duplicate event in persisted requests. %d %+v", i, op))
-				}
-				hasPut = true
-				persisted[kv] = lastReturnTime
-			}
-			if hasPut {
-				newReturnTime := requestReturnTime(operationReturnTime, request)
-				if newReturnTime != -1 {
-					lastReturnTime = min(lastReturnTime, newReturnTime)
-				}
-			}
-		case model.LeaseGrant:
-		case model.LeaseRevoke:
-		case model.Compact:
-		default:
-			panic(fmt.Sprintf("Unknown request type: %q", request.Type))
-		}
-	}
-	return persisted
-}
-
-func operationReturnTime(operations []porcupine.Operation) map[model.EtcdOperation]int64 {
-	newOperations := map[model.EtcdOperation]int64{}
-	for _, op := range operations {
+func returnTime(allOperations []porcupine.Operation, reports []report.ClientReport, persistedRequests []model.EtcdRequest) map[keyValue]int64 {
+	earliestReturnTime := map[keyValue]int64{}
+	var lastReturnTime int64 = 0
+	for _, op := range allOperations {
 		request := op.Input.(model.EtcdRequest)
 		switch request.Type {
 		case model.Txn:
@@ -173,10 +131,11 @@ func operationReturnTime(operations []porcupine.Operation) map[model.EtcdOperati
 				if etcdOp.Type != model.PutOperation {
 					continue
 				}
-				if _, found := newOperations[etcdOp]; found {
+				kv := keyValue{Key: etcdOp.Put.Key, Value: etcdOp.Put.Value}
+				if _, found := earliestReturnTime[kv]; found {
 					panic("Unexpected duplicate event in persisted requests.")
 				}
-				newOperations[etcdOp] = op.Return
+				earliestReturnTime[kv] = op.Return
 			}
 		case model.Range:
 		case model.LeaseGrant:
@@ -185,39 +144,11 @@ func operationReturnTime(operations []porcupine.Operation) map[model.EtcdOperati
 		default:
 			panic(fmt.Sprintf("Unknown request type: %q", request.Type))
 		}
-	}
-	return newOperations
-}
-
-func maxReturnTime(operationTime map[model.EtcdOperation]int64) int64 {
-	var maxReturnTime int64
-	for _, returnTime := range operationTime {
-		if returnTime > maxReturnTime {
-			maxReturnTime = returnTime
+		if op.Return > lastReturnTime {
+			lastReturnTime = op.Return
 		}
 	}
-	return maxReturnTime
-}
 
-func requestReturnTime(operationTime map[model.EtcdOperation]int64, request model.EtcdRequest) int64 {
-	switch request.Type {
-	case model.Txn:
-		for _, op := range append(request.Txn.OperationsOnSuccess, request.Txn.OperationsOnFailure...) {
-			if op.Type != model.PutOperation {
-				continue
-			}
-			if time, found := operationTime[op]; found {
-				return time
-			}
-		}
-		return -1
-	default:
-		panic(fmt.Sprintf("Unknown request type: %q", request.Type))
-	}
-}
-
-func watchReturnTime(reports []report.ClientReport) map[keyValue]int64 {
-	earliestTime := map[keyValue]int64{}
 	for _, client := range reports {
 		for _, watch := range client.Watch {
 			for _, resp := range watch.Responses {
@@ -227,8 +158,8 @@ func watchReturnTime(reports []report.ClientReport) map[keyValue]int64 {
 					case model.RangeOperation:
 					case model.PutOperation:
 						kv := keyValue{Key: event.Key, Value: event.Value}
-						if t, ok := earliestTime[kv]; !ok || t > resp.Time.Nanoseconds() {
-							earliestTime[kv] = resp.Time.Nanoseconds()
+						if t, ok := earliestReturnTime[kv]; !ok || t > resp.Time.Nanoseconds() {
+							earliestReturnTime[kv] = resp.Time.Nanoseconds()
 						}
 					case model.DeleteOperation:
 					default:
@@ -238,7 +169,63 @@ func watchReturnTime(reports []report.ClientReport) map[keyValue]int64 {
 			}
 		}
 	}
-	return earliestTime
+
+	for i := len(persistedRequests) - 1; i >= 0; i-- {
+		request := persistedRequests[i]
+		switch request.Type {
+		case model.Txn:
+			lastReturnTime--
+			for _, op := range request.Txn.OperationsOnSuccess {
+				if op.Type != model.PutOperation {
+					continue
+				}
+				kv := keyValue{Key: op.Put.Key, Value: op.Put.Value}
+				returnTime, ok := earliestReturnTime[kv]
+				if ok {
+					lastReturnTime = min(returnTime, lastReturnTime)
+					earliestReturnTime[kv] = lastReturnTime
+				}
+			}
+		case model.LeaseGrant:
+		case model.LeaseRevoke:
+		case model.Compact:
+		default:
+			panic(fmt.Sprintf("Unknown request type: %q", request.Type))
+		}
+	}
+	return earliestReturnTime
+}
+
+func countPersistedRequests(requests []model.EtcdRequest) map[keyValue]int64 {
+	counter := map[keyValue]int64{}
+	for _, request := range requests {
+		countRequest(counter, request)
+	}
+	return counter
+}
+
+func countRequest(counter map[keyValue]int64, request model.EtcdRequest) {
+	switch request.Type {
+	case model.Txn:
+		for _, operation := range append(request.Txn.OperationsOnSuccess, request.Txn.OperationsOnFailure...) {
+			switch operation.Type {
+			case model.PutOperation:
+				kv := keyValue{Key: operation.Put.Key, Value: operation.Put.Value}
+				counter[kv] += 1
+			case model.DeleteOperation:
+			case model.RangeOperation:
+			default:
+				panic(fmt.Sprintf("unknown operation type %q", operation.Type))
+			}
+		}
+	case model.LeaseGrant:
+	case model.LeaseRevoke:
+	case model.Compact:
+	case model.Defragment:
+	case model.Range:
+	default:
+		panic(fmt.Sprintf("unknown request type %q", request.Type))
+	}
 }
 
 func requestRevision(reports []report.ClientReport) map[keyValue]int64 {
