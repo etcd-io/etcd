ae27db3ed4bf011d7c8c945703de9c068431633f
Benjamin Wang <benjamin.ahrtr@gmail.com>
2024-10-24 16:24:39 +0100

    Define a DLocker interface for distributed locker
    Also updated the contrib/lock example to demo how to use the DLocker
    
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- client/v3/concurrency/mutex.go
+++ client/v3/concurrency/mutex.go
@@ -152,6 +152,7 @@ func (m *Mutex) IsOwner() v3.Cmp {
 }
 
 func (m *Mutex) Key() string { return m.myKey }
+func (m *Mutex) Rev() int64  { return m.myRev }
 
 // Header is the response header received from etcd on acquiring the lock.
 func (m *Mutex) Header() *pb.ResponseHeader { return m.hdr }
@@ -171,7 +172,17 @@ func (lm *lockerMutex) Unlock() {
 	}
 }
 
-// NewLocker creates a sync.Locker backed by an etcd mutex.
-func NewLocker(s *Session, pfx string) sync.Locker {
+// NewLocker creates a DLocker backed by an etcd mutex.
+func NewLocker(s *Session, pfx string) DLocker {
 	return &lockerMutex{NewMutex(s, pfx)}
+}
+
+// DLocker represents an object that can be locked and unlocked
+// in distributed environment.
+type DLocker interface {
+	sync.Locker
+	// Rev returns a revision which is monotonically incremental. It can
+	// be used as a fencing token to prevent expired locker from operating
+	// the shared resource.
+	Rev() int64
 }
