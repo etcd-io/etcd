a3bd030b34286246828c7850d9a4d0425a91307c
Benjamin Wang <benjamin.ahrtr@gmail.com>
2024-12-12 21:45:28 +0000

    Do not wait for ready notify if the server is stopping
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- embed/serve.go
+++ embed/serve.go
@@ -16,6 +16,7 @@ package embed
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	defaultLog "log"
@@ -95,6 +96,12 @@ func (sctx *serveCtx) serve(
 	gopts ...grpc.ServerOption) (err error) {
 	logger := defaultLog.New(ioutil.Discard, "etcdhttp", 0)
 	<-s.ReadyNotify()
+
+	select {
+	case <-s.StoppingNotify():
+		return errors.New("server is stopping")
+	case <-s.ReadyNotify():
+	}
 
 	if sctx.lg != nil {
 		sctx.lg.Info("ready to serve client requests")
