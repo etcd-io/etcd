c342fc6e2bb60337f73c48d42788c0be7f07bc0e
Ivan Valdes <ivan@vald.es>
2024-12-17 12:25:26 -0800

    Migrate updatebom.sh to use the Go workspace
    Modify the `bom_fix` function to call `bom_fixlet` directly instead of
    running it for a specific module.
    
    Update the `bom_fixlet` function to use a Bash compatible replacement
    for reading module lines.
    
    Simplify the logic for generating the bill of materials.
    
    Signed-off-by: Ivan Valdes <ivan@vald.es>

--- scripts/updatebom.sh
+++ scripts/updatebom.sh
@@ -10,11 +10,11 @@ function bom_fixlet {
   cp go.mod go.mod.tmp
   cp go.sum go.sum.tmp
 
-  local modules
-  # shellcheck disable=SC2207
-  modules=($(modules_for_bom))
+  # bash 3.x compatible replacement of: mapfile -t modules < <(workspace_modules_without_tools)
+  local modules=()
+  while IFS= read -r line; do modules+=("$line"); done < <(workspace_relative_modules_without_tools)
 
-  if GOFLAGS=-mod=mod run_go_tool "github.com/appscodelabs/license-bill-of-materials" \
+  if run_go_tool "github.com/appscodelabs/license-bill-of-materials" \
       --override-file ./bill-of-materials.override.json \
       "${modules[@]}" > ./bill-of-materials.json.tmp; then
     cp ./bill-of-materials.json.tmp ./bill-of-materials.json
@@ -30,10 +30,7 @@ function bom_fixlet {
 }
 
 function bom_fix {
-  # We regenerate bom from the tests directory, as it's a module
-  # that depends on all other modules, so we can generate comprehensive content.
-  # TODO: Migrate to root module, when root module depends on everything (including server & tests).
-  run_for_module "." bom_fixlet
+  run bom_fixlet
 }
 
 # only build when called directly, not sourced
