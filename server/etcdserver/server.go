c1ab521a28d1347b0f676f00e869ae4e58e424e2
Benjamin Wang <benjamin.ahrtr@gmail.com>
2025-01-08 10:16:50 +0000

    Fix mixVersion test case: ensure a snapshot to be sent out
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- server/etcdserver/server.go
+++ server/etcdserver/server.go
@@ -623,7 +623,7 @@ func NewServer(cfg config.ServerConfig) (srv *EtcdServer, err error) {
 	srv.kv = mvcc.New(srv.Logger(), srv.be, srv.lessor, mvccStoreConfig)
 
 	kvindex := ci.ConsistentIndex()
-	srv.lg.Debug("restore consistentIndex", zap.Uint64("index", kvindex))
+	srv.lg.Info("restore consistentIndex", zap.Uint64("index", kvindex))
 
 	if beExist {
 		// TODO: remove kvindex != 0 checking when we do not expect users to upgrade
