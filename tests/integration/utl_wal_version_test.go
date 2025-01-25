7a3824904ee8d443cbbe14d517e86191056faa61
Benjamin Wang <benjamin.ahrtr@gmail.com>
2025-01-25 23:42:31 +0000

    Enhance TestEtcdVersionFromWAL to support downgradeVersionTest
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- tests/integration/utl_wal_version_test.go
+++ tests/integration/utl_wal_version_test.go
@@ -28,80 +28,85 @@ import (
 	"go.etcd.io/etcd/server/v3/embed"
 	"go.etcd.io/etcd/server/v3/storage/wal"
 	"go.etcd.io/etcd/server/v3/storage/wal/walpb"
-	framecfg "go.etcd.io/etcd/tests/v3/framework/config"
 	"go.etcd.io/etcd/tests/v3/framework/integration"
 )
 
 func TestEtcdVersionFromWAL(t *testing.T) {
-	testutil.SkipTestIfShortMode(t,
-		"Wal creation tests are depending on embedded etcd server so are integration-level tests.")
-	cfg := integration.NewEmbedConfig(t, "default")
-	srv, err := embed.StartEtcd(cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	select {
-	case <-srv.Server.ReadyNotify():
-	case <-time.After(3 * time.Second):
-		t.Fatalf("failed to start embed.Etcd for test")
-	}
-
-	// When the member becomes leader, it will update the cluster version
-	// with the cluster's minimum version. As it's updated asynchronously,
-	// it could not be updated in time before close. Wait for it to become
-	// ready.
-	if err = waitForClusterVersionReady(srv); err != nil {
-		srv.Close()
-		t.Fatalf("failed to wait for cluster version to become ready: %v", err)
-	}
-
-	ccfg := clientv3.Config{Endpoints: []string{cfg.AdvertiseClientUrls[0].String()}}
-	cli, err := integration.NewClient(t, ccfg)
-	if err != nil {
-		srv.Close()
-		t.Fatal(err)
-	}
-
-	// Once the cluster version has been updated, any entity's storage
-	// version should be align with cluster version.
-	ctx, cancel := context.WithTimeout(context.Background(), testutil.RequestTimeout)
-	_, err = cli.AuthStatus(ctx)
-	cancel()
-	if err != nil {
-		srv.Close()
-		t.Fatalf("failed to get auth status: %v", err)
+	testCases := []struct {
+		name               string
+		operation          func(ctx context.Context, c *clientv3.Client) error
+		expectedWALVersion *semver.Version
+	}{
+		{
+			name: "normal operation",
+			operation: func(ctx context.Context, c *clientv3.Client) error {
+				_, err := c.AuthStatus(ctx)
+				return err
+			},
+			expectedWALVersion: &semver.Version{Major: 3, Minor: 5},
+		},
+		{
+			name: "downgrade version 3.6 test operation",
+			operation: func(ctx context.Context, c *clientv3.Client) error {
+				_, err := c.DowngradeVersionTest(ctx, "3.6.0")
+				return err
+			},
+			expectedWALVersion: &semver.Version{Major: 3, Minor: 6},
+		},
+		{
+			name: "downgrade version 3.7 test operation",
+			operation: func(ctx context.Context, c *clientv3.Client) error {
+				_, err := c.DowngradeVersionTest(ctx, "3.7.0")
+				return err
+			},
+			expectedWALVersion: &semver.Version{Major: 3, Minor: 7},
+		},
 	}
 
-	cli.Close()
-	srv.Close()
-
-	w, err := wal.Open(zap.NewNop(), cfg.Dir+"/member/wal", walpb.Snapshot{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer w.Close()
-
-	walVersion, err := wal.ReadWALVersion(w)
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, &semver.Version{Major: 3, Minor: 5}, walVersion.MinimalEtcdVersion())
-}
-
-func waitForClusterVersionReady(srv *embed.Etcd) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	for {
+	for _, tc := range testCases {
+		testutil.SkipTestIfShortMode(t,
+			"Wal creation tests are depending on embedded etcd server so are integration-level tests.")
+		cfg := integration.NewEmbedConfig(t, "default")
+		srv, err := embed.StartEtcd(cfg)
+		if err != nil {
+			t.Fatal(err)
+		}
 		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
+		case <-srv.Server.ReadyNotify():
+		case <-time.After(3 * time.Second):
+			t.Fatalf("failed to start embed.Etcd for test")
 		}
 
-		if srv.Server.ClusterVersion() != nil {
-			return nil
+		ccfg := clientv3.Config{Endpoints: []string{cfg.AdvertiseClientUrls[0].String()}}
+		cli, err := integration.NewClient(t, ccfg)
+		if err != nil {
+			srv.Close()
+			t.Fatal(err)
 		}
-		time.Sleep(framecfg.TickDuration)
+
+		// Once the cluster version has been updated, any entity's storage
+		// version should be align with cluster version.
+		ctx, cancel := context.WithTimeout(context.Background(), testutil.RequestTimeout)
+		err = tc.operation(ctx, cli)
+		cancel()
+		if err != nil {
+			srv.Close()
+			t.Fatalf("failed to get auth status: %v", err)
+		}
+
+		cli.Close()
+		srv.Close()
+
+		w, err := wal.Open(zap.NewNop(), cfg.Dir+"/member/wal", walpb.Snapshot{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer w.Close()
+
+		walVersion, err := wal.ReadWALVersion(w)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, tc.expectedWALVersion, walVersion.MinimalEtcdVersion())
 	}
 }
