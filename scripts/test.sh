d3c2c0bf35e8a5727b14ca4cba902bf2f527f592
Ivan Valdes <ivan@vald.es>
2025-02-14 21:23:44 -0800

    Migrate bom command to use a Go workspace
    Remove `GOFLAGS=-mod=mod` from the execution of
    license-bill-of-materials.
    
    Signed-off-by: Ivan Valdes <ivan@vald.es>

--- scripts/test.sh
+++ scripts/test.sh
@@ -483,7 +483,7 @@ function bom_pass {
   run cp go.sum go.sum.tmp || return 2
   run cp go.mod go.mod.tmp || return 2
 
-  output=$(GOFLAGS=-mod=mod run_go_tool github.com/appscodelabs/license-bill-of-materials \
+  output=$(run_go_tool github.com/appscodelabs/license-bill-of-materials \
     --override-file ./bill-of-materials.override.json \
     "${modules[@]}")
   code="$?"
