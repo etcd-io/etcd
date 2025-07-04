345f4f895c8a4f027f1c0850fa0e276bc54e4a7f
Peter Chang <peter.yaochen.chang@gmail.com>
2025-07-04 20:58:36 +0000

    cache: remove AfterRev entry predicate
    Signed-off-by: Peter Chang <peter.yaochen.chang@gmail.com>

--- cache/predicate.go
+++ cache/predicate.go
@@ -14,8 +14,6 @@
 
 package cache
 
-import clientv3 "go.etcd.io/etcd/client/v3"
-
 type Prefix string
 
 func (prefix Prefix) Match(key []byte) bool {
@@ -24,11 +22,4 @@ func (prefix Prefix) Match(key []byte) bool {
 	}
 	prefixLen := len(prefix)
 	return len(key) >= prefixLen && string(key[:prefixLen]) == string(prefix)
-}
-
-// AfterRev builds an EntryPredicate that matches events whose ModRevision ≥ rev.
-func AfterRev(rev int64) EntryPredicate {
-	return func(ev *clientv3.Event) bool {
-		return ev.Kv.ModRevision >= rev
-	}
 }
