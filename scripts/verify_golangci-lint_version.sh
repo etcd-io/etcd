e1f9fc4e84b96287697bcc5a9a34f2957cb5f6a6
sprochazka <s.prochazka6@gmail.com>
2025-07-14 10:57:37 +0000

    Tooling: Added licence header
    changed the shebang command to mathc other scripts
    
    Signed-off-by: sprochazka <s.prochazka6@gmail.com>

--- scripts/verify_golangci-lint_version.sh
+++ scripts/verify_golangci-lint_version.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Copyright 2015 The etcd Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 function install_golangci_lint() {
     echo "Installing golangci-lint ${GOLANGCI_LINT_VERSION}"
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${GOPATH}/bin" "${GOLANGCI_LINT_VERSION}"
