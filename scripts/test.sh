8b0241630167ba48133c7b9a9bfb149e8af15c2e
Ivan Valdes <ivan@vald.es>
2025-10-03 15:03:34 -0700

    Fix running BOM on fresh clones
    Signed-off-by: Ivan Valdes <ivan@vald.es>

--- scripts/test.sh
+++ scripts/test.sh
@@ -516,8 +516,13 @@ function bom_pass {
   run cp go.sum go.sum.tmp || return 2
   run cp go.mod go.mod.tmp || return 2
 
+  # Intentionally run the command once first, so it fetches dependencies. The exit code on the first
+  # run in a just cloned repository is always dirty.
+  GOOS=linux run_go_tool github.com/appscodelabs/license-bill-of-materials \
+    --override-file ./bill-of-materials.override.json "${modules[@]}"
+
   # BOM file should be generated for linux. Otherwise running this command on other operating systems such as OSX
-  # results in certain dependencies being excluded from the BOM file, such as procfs. 
+  # results in certain dependencies being excluded from the BOM file, such as procfs.
   # For more info, https://github.com/etcd-io/etcd/issues/19665
   output=$(GOOS=linux run_go_tool github.com/appscodelabs/license-bill-of-materials \
     --override-file ./bill-of-materials.override.json \
