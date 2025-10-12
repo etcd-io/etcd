5377bb9d811038134ae41ae7edd365b659b7a153
xUser5000 <abdallahar1974@gmail.com>
2025-10-12 23:20:16 +0300

    etcdserver: fix cannot promote with auth from follower
    When auth is enabled, sending a promotion request to a follower node was failing because
    the auth token was not being propagated when the follower forwards the request to the leader.
    
    Signed-off-by: xUser5000 <abdallahar1974@gmail.com>

--- server/etcdserver/cluster_util.go
+++ server/etcdserver/cluster_util.go
@@ -27,7 +27,9 @@ import (
 
 	"github.com/coreos/go-semver/semver"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/metadata"
 
+	"go.etcd.io/etcd/api/v3/v3rpc/rpctypes"
 	"go.etcd.io/etcd/api/v3/version"
 	"go.etcd.io/etcd/client/pkg/v3/types"
 	"go.etcd.io/etcd/server/v3/etcdserver/api/membership"
@@ -305,6 +307,14 @@ func promoteMemberHTTP(ctx context.Context, url string, id uint64, peerRt http.R
 	if err != nil {
 		return nil, err
 	}
+
+	// add the auth token via HTTP header if present in gRPC metadata
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		if ts := md.Get(rpctypes.TokenFieldNameGRPC); len(ts) > 0 {
+			req.Header.Set("Authorization", ts[0])
+		}
+	}
+
 	req = req.WithContext(ctx)
 	resp, err := cc.Do(req)
 	if err != nil {
