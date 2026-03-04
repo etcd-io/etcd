565e41aa7172ea2b99c1f0c632bc4aa6ee8b5896
Chun-Hung Tseng <henrytseng@google.com>
2026-03-04 12:31:56 +0100

    robustness: reproduce #20221
    Targeting bug fix https://github.com/etcd-io/etcd/pull/20229
    
    Signed-off-by: Chun-Hung Tseng <henrytseng@google.com>

--- tests/robustness/scenarios/scenarios.go
+++ tests/robustness/scenarios/scenarios.go
@@ -308,6 +308,29 @@ func Regression(t *testing.T) []TestScenario {
 			e2e.WithGoFailEnabled(true),
 		),
 	})
+	scenarios = append(scenarios, TestScenario{
+		Name:      "Issue20221",
+		Failpoint: failpoint.BlackholeUntilSnapshot,
+		Watch: client.WatchConfig{
+			RequestProgress: true,
+		},
+		Profile: traffic.Profile{
+			KeyValue: &traffic.KeyValueHigh,
+			Watch: &traffic.Watch{
+				MemberClientCount:   6,
+				ClusterClientCount:  2,
+				RevisionOffsetRange: traffic.Range{Min: 50, Max: 100},
+			},
+		},
+		Traffic: traffic.EtcdPut,
+		Cluster: *e2e.NewConfig(
+			e2e.WithSnapshotCount(10),
+			e2e.WithPeerProxy(true),
+			e2e.WithIsPeerTLS(true),
+			e2e.WithWatchProcessNotifyInterval(10*time.Millisecond),
+			e2e.WithSnapshotCatchUpEntries(10),
+		),
+	})
 	if v.Compare(version.V3_5) >= 0 {
 		opts := []e2e.EPClusterOption{
 			e2e.WithSnapshotCount(100),
