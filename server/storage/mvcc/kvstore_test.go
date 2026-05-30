dd57ad39fa4eb1afcd9abdadf75354bca0600d9f
Nont <9658731+nwnt@users.noreply.github.com>
2026-05-29 23:51:59 -0500

    Add support with limit and withTotalCount for fastKeysOnly Range
    Signed-off-by: Nont <9658731+nwnt@users.noreply.github.com>

--- server/storage/mvcc/kvstore_test.go
+++ server/storage/mvcc/kvstore_test.go
@@ -201,11 +201,11 @@ func TestStoreRange(t *testing.T) {
 		r    rangeResp
 	}{
 		{
-			indexRangeResp{[][]byte{[]byte("foo")}, []Revision{{Main: 2}}, []Revision{{Main: 1}}, []int64{1}},
+			indexRangeResp{[][]byte{[]byte("foo")}, []Revision{{Main: 2}}, []Revision{{Main: 1}}, []int64{1}, 1},
 			rangeResp{[][]byte{key}, [][]byte{kvb}},
 		},
 		{
-			indexRangeResp{[][]byte{[]byte("foo"), []byte("foo1")}, []Revision{{Main: 2}, {Main: 3}}, []Revision{{Main: 2}, {Main: 3}}, []int64{1, 1}},
+			indexRangeResp{[][]byte{[]byte("foo"), []byte("foo1")}, []Revision{{Main: 2}, {Main: 3}}, []Revision{{Main: 2}, {Main: 3}}, []int64{1, 1}, 2},
 			rangeResp{[][]byte{key}, [][]byte{kvb}},
 		},
 	}
@@ -280,7 +280,7 @@ func TestStoreDeleteRange(t *testing.T) {
 	}{
 		{
 			Revision{Main: 2},
-			indexRangeResp{[][]byte{[]byte("foo")}, []Revision{{Main: 2}}, []Revision{{Main: 2}}, []int64{1}},
+			indexRangeResp{[][]byte{[]byte("foo")}, []Revision{{Main: 2}}, []Revision{{Main: 2}}, []int64{1}, 1},
 			rangeResp{[][]byte{key}, [][]byte{kvb}},
 
 			newTestBucketKeyBytes(newBucketKey(3, 0, true)),
@@ -1018,6 +1018,7 @@ type indexRangeResp struct {
 	revs     []Revision
 	creates  []Revision
 	versions []int64
+	total    int
 }
 
 type indexRangeEventsResp struct {
@@ -1033,7 +1034,7 @@ type fakeIndex struct {
 }
 
 func (i *fakeIndex) Revisions(key, end []byte, atRev int64, limit int, withTotalCount bool) ([]Revision, int) {
-	_, rev, _, _ := i.Range(key, end, atRev)
+	_, rev, _, _, _ := i.Range(key, end, atRev, limit, withTotalCount)
 	if len(rev) >= limit {
 		rev = rev[:limit]
 	}
@@ -1041,7 +1042,7 @@ func (i *fakeIndex) Revisions(key, end []byte, atRev int64, limit int, withTotal
 }
 
 func (i *fakeIndex) CountRevisions(key, end []byte, atRev int64) int {
-	_, rev, _, _ := i.Range(key, end, atRev)
+	_, rev, _, _, _ := i.Range(key, end, atRev, 0, true)
 	return len(rev)
 }
 
@@ -1051,10 +1052,10 @@ func (i *fakeIndex) Get(key []byte, atRev int64) (rev, created Revision, ver int
 	return r.rev, r.created, r.ver, r.err
 }
 
-func (i *fakeIndex) Range(key, end []byte, atRev int64) (keys [][]byte, modifies, creates []Revision, versions []int64) {
+func (i *fakeIndex) Range(key, end []byte, atRev int64, limit int, withTotalCount bool) (keys [][]byte, modifies, creates []Revision, versions []int64, total int) {
 	i.Recorder.Record(testutil.Action{Name: "range", Params: []any{key, end, atRev}})
 	r := <-i.indexRangeRespc
-	return r.keys, r.revs, r.creates, r.versions
+	return r.keys, r.revs, r.creates, r.versions, r.total
 }
 
 func (i *fakeIndex) Put(key []byte, rev Revision) {
