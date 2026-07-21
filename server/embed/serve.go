8d794ce7335557f4310a4c42c2241aab5a40e4d9
Benjamin Wang <benjamin.ahrtr@gmail.com>
2026-07-21 09:47:16 +0100

    Set a 5-minutes ReadHeaderTimeout for client http.Server
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- server/embed/serve.go
+++ server/embed/serve.go
@@ -24,6 +24,7 @@ import (
 	"net/http"
 	"strings"
 	"sync"
+	"time"
 
 	etcdservergw "go.etcd.io/etcd/api/v3/etcdserverpb/gw"
 	"go.etcd.io/etcd/client/pkg/v3/transport"
@@ -159,8 +160,9 @@ func (sctx *serveCtx) serve(
 		if httpEnabled {
 			httpmux := sctx.createMux(gwmux, handler)
 			srv = &http.Server{
-				Handler:  createAccessController(sctx.lg, s, httpmux),
-				ErrorLog: logger, // do not log user error
+				Handler:           createAccessController(sctx.lg, s, httpmux),
+				ReadHeaderTimeout: 5 * time.Minute,
+				ErrorLog:          logger, // do not log user error
 			}
 			if err := configureHttpServer(srv, s.Cfg); err != nil {
 				sctx.lg.Error("Configure http server failed", zap.Error(err))
@@ -253,9 +255,10 @@ func (sctx *serveCtx) serve(
 			httpmux := sctx.createMux(gwmux, handler)
 
 			srv = &http.Server{
-				Handler:   createAccessController(sctx.lg, s, httpmux),
-				TLSConfig: tlscfg,
-				ErrorLog:  logger, // do not log user error
+				Handler:           createAccessController(sctx.lg, s, httpmux),
+				TLSConfig:         tlscfg,
+				ReadHeaderTimeout: 5 * time.Minute,
+				ErrorLog:          logger, // do not log user error
 			}
 			if err := configureHttpServer(srv, s.Cfg); err != nil {
 				sctx.lg.Error("Configure https server failed", zap.Error(err))
